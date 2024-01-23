076a7f56c88a0239ad9d58c902d14e74b77361dd
Pedro Nascimento <pedrovitor.marques@gmail.com>
2024-01-23 16:50:56 -0300

    Implementando dependências via set_upstream

--- pipelines/prontuarios/raw/smsrio/flows.py
+++ pipelines/prontuarios/raw/smsrio/flows.py
@@ -41,22 +41,35 @@ with Flow(
     # Parameters
     #####################################
     ENVIRONMENT=Parameter("environment", default="dev")
+
     CNES=Parameter("cnes")
+
     RENAME_FLOW = Parameter("rename_flow", default=False)
 
     #####################################
     # Set environment
     ####################################
-    database_url=get_database_url(ENVIRONMENT)
-    api_token=get_api_token(ENVIRONMENT)
+    database_url=get_database_url(
+        environment=ENVIRONMENT
+    )
+
+    api_token=get_api_token(
+        environment=ENVIRONMENT
+    )
+    api_token.set_upstream(database_url)
 
     with case(RENAME_FLOW, True):
-        rename_current_flow_run(ENVIRONMENT, CNES)
+        rename_flow_task = rename_current_flow_run(
+            environment=ENVIRONMENT,
+            cnes=CNES
+        )
+        rename_flow_task.set_upstream(api_token)
 
     ####################################
     # Task Section #1 - Get data
     ####################################
     window_start, window_end=get_scheduled_window()
+    window_start.set_upstream(api_token)
 
     patient_data=extract_tabledata_from_db(
         db_url=database_url,
@@ -65,6 +78,7 @@ with Flow(
         max_date=window_end,
         date_lookup_field="timestamp",
     )
+    patient_data.set_upstream(window_start)
 
     cns_data=extract_tabledata_from_db(
         db_url=database_url,
@@ -73,43 +87,56 @@ with Flow(
         max_date=window_end,
         date_lookup_field="timestamp",
     )
+    cns_data.set_upstream(patient_data)
 
     ####################################
     # Task Section #2 - Transform and merge data
     ####################################
-    patient_data=transform_standardize_columns_names(
+    standardized_patient_data=transform_standardize_columns_names(
         dataframe=patient_data,
         columns_map={'cpf': 'patient_cpf'}
     )
+    standardized_patient_data.set_upstream(cns_data)
 
-    patient_data=transform_filter_invalid_cpf(
-        dataframe=patient_data,
+    valid_patient_data=transform_filter_invalid_cpf(
+        dataframe=standardized_patient_data,
         cpf_column='patient_cpf'
     )
+    valid_patient_data.set_upstream(standardized_patient_data)
 
-    merged_data=transform_merge_patient_and_cns_data(
-        patient_data=patient_data,
+    merged_patient_data=transform_merge_patient_and_cns_data(
+        patient_data=valid_patient_data,
         cns_data=cns_data,
     )
+    merged_patient_data.set_upstream(valid_patient_data)
 
     ####################################
     # Task Section #3 - Prepare data to load
     ####################################
-    json_list=transform_data_to_json(merged_data)
+    json_list=transform_data_to_json(
+        dataframe=merged_patient_data,
+        identifier_column="patient_cpf"
+    )
+    json_list.set_upstream(merged_patient_data)
 
-    json_list_batches=transform_create_input_batches(json_list)
+    json_list_batches=transform_create_input_batches(
+        json_list
+    )
+    json_list_batches.set_upstream(json_list)
 
     request_bodies=transform_to_raw_format.map(
         json_list_batches,
         unmapped(CNES),
     )
+    request_bodies.set_upstream(json_list_batches)
 
-    load_to_api.map(
+    load_to_api_task = load_to_api.map(
         request_body=request_bodies,
         endpoint_name=unmapped("raw/patientrecords"),
         api_token=unmapped(api_token),
         environment=unmapped(ENVIRONMENT),
     )
+    load_to_api_task.set_upstream(request_bodies)
 
 
 sms_prontuarios_raw_smsrio.storage=GCS(constants.GCS_FLOWS_BUCKET.value)
@@ -122,4 +149,4 @@ sms_prontuarios_raw_smsrio.run_config=KubernetesRun(
     memory_limit="2Gi"
 )
 
-sms_prontuarios_raw_smsrio.schedule=smsrio_daily_update_schedule
+#sms_prontuarios_raw_smsrio.schedule=smsrio_daily_update_schedule
