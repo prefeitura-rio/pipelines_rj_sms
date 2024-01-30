1583cf856598c9d1b55434831dabea080985f22e
Pedro Marques <pedrovitor.marques@gmail.com>
2024-01-30 15:56:19 -0300

    Adding run config

--- pipelines/tests/flows.py
+++ pipelines/tests/flows.py
@@ -1,4 +1,9 @@
 from prefeitura_rio.pipelines_utils.custom import Flow
+from prefect import Parameter
+from prefect.run_configs import KubernetesRun
+from prefect.storage import GCS
+from pipelines.constants import constants
+from prefect.executors import LocalDaskExecutor
 from pipelines.utils.tasks import (
     inject_gcp_credentials
 )
@@ -17,3 +22,13 @@ with Flow(
     list_all_secrets_name(environment=ENVIRONMENT)
 
     inject_gcp_credentials(environment=ENVIRONMENT)
+
+test_ambiente.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
+test_ambiente.executor = LocalDaskExecutor(num_workers=10)
+test_ambiente.run_config = KubernetesRun(
+    image=constants.DOCKER_IMAGE.value,
+    labels=[
+        constants.RJ_SMS_AGENT_LABEL.value,
+    ],
+    memory_limit="2Gi"
+)
