e81dbad7360aa52fcc06a7898fbe7c02f76f6f4c
ThiagoTrabach <thiago.trabach@gmail.com>
2024-02-01 15:38:20 -0300

    Add max retries and timeout to download_ftp task
    Update URL in poetry.lock
    
    Update URL in pyproject.toml

--- pipelines/utils/tasks.py
+++ pipelines/utils/tasks.py
@@ -249,7 +249,7 @@ def download_azure_blob(
     return destination_file_path
 
 
-@task
+@task(max_retries=3, retry_delay= timedelta(seconds=5), timeout=timedelta(seconds=240))
 def download_ftp(
     host: str,
     user: str,
