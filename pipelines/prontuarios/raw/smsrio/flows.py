78a4bc1cbc7be8c1aa3f519ffa7e7d41f162769a
Pedro Marques <pedrovitor.marques@gmail.com>
2024-02-02 11:45:26 -0300

    increasing available memory

--- pipelines/prontuarios/raw/smsrio/flows.py
+++ pipelines/prontuarios/raw/smsrio/flows.py
@@ -132,7 +132,7 @@ sms_prontuarios_raw_smsrio.run_config = KubernetesRun(
     labels=[
         constants.RJ_SMS_AGENT_LABEL.value,
     ],
-    memory_limit="4Gi",
+    memory_limit="8Gi",
 )
 
 sms_prontuarios_raw_smsrio.schedule = smsrio_daily_update_schedule
