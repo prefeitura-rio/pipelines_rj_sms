c3428779bf20ad941fb9aeff3e6bb17500ff9331
Pedro Marques <pedrovitor.marques@gmail.com>
2024-02-02 16:37:40 -0300

    Returning missing task

--- pipelines/prontuarios/utils/tasks.py
+++ pipelines/prontuarios/utils/tasks.py
@@ -168,3 +168,18 @@ def transform_split_dataframe(
 ) -> list[pd.DataFrame]:
 
     return split_dataframe(dataframe, chunk_size=batch_size)
+
+
+@task
+def transform_create_input_batches(input_list: list, batch_size: int = 250):
+    """
+    Transform input list into batches
+
+    Args:
+        input_list (list): Input list
+        batch_size (int, optional): Batch size. Defaults to 250.
+
+    Returns:
+        list[list]: List of batches
+    """
+    return [input_list[i : i + batch_size] for i in range(0, len(input_list), batch_size)]
