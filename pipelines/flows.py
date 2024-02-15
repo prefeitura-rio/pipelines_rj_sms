6e4f2a221b1c499341576d088f07c4a5181cf594
Pedro Nascimento <pedrovitor.marques@gmail.com>
2024-02-15 14:18:13 -0300

    Update import statement in flows.py

--- pipelines/flows.py
+++ pipelines/flows.py
@@ -12,4 +12,4 @@ from pipelines.dump_db_smsrio.flows import *
 from pipelines.dump_ftp_cnes.flows import *
 from pipelines.execute_dbt.flows import *
 from pipelines.prontuarios.raw.smsrio.flows import *
-from pipelines.prontuarios.raw.vitai.rotineiro.flows import *
+from pipelines.prontuarios.raw.vitai.flows import *
