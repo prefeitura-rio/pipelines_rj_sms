16504ddd99bc38a5f6fe92a7f57ddb6c02eb4e2b
Pedro Nascimento <pedrovitor.marques@gmail.com>
2024-02-21 18:14:29 -0300

    Add logging to group_data_by_cpf function

--- pipelines/prontuarios/raw/vitai/utils.py
+++ pipelines/prontuarios/raw/vitai/utils.py
@@ -2,6 +2,7 @@
 """
 Utilities functions for Vitai Raw Data Extraction
 """
+import prefect
 from datetime import date
 from typing import Callable
 
@@ -32,8 +33,20 @@ def group_data_by_cpf(data_list: list, cpf_get_function: Callable[[str], str]) -
         dict: A dictionary containing the grouped data, where each key is the patient CPF
               and the value is the corresponding data item.
     """
+    logger = prefect.context.get("logger")
+
     groups = []
     for data in data_list:
-        group = {"patient_cpf": cpf_get_function(data), "data": data}
+        try:
+            patient_cpf = cpf_get_function(data)
+        except TypeError as e:
+            logger.warning(f"Skipping data item: {data}. Reason: {e}")
+            continue
+
+        group = {
+            "patient_cpf": patient_cpf, 
+            "data": data
+        }
         groups.append(group)
+
     return groups
