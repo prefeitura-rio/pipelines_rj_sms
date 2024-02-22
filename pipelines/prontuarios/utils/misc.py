171df412b1b47d5cca0071a461ead78815353b54
Pedro Nascimento <pedrovitor.marques@gmail.com>
2024-02-22 15:10:54 -0300

    Add logging for data item skipping

--- pipelines/prontuarios/utils/misc.py
+++ pipelines/prontuarios/utils/misc.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from typing import Callable
 
+import prefect
+
 
 def split_dataframe(df, chunk_size=10000):
     chunks = list()
@@ -23,8 +25,15 @@ def group_data_by_cpf(data_list: list, cpf_get_function: Callable[[str], str]) -
         dict: A dictionary containing the grouped data, where each key is the patient CPF
               and the value is the corresponding data item.
     """
+    logger = prefect.context.get("logger")
+
     groups = []
     for data in data_list:
-        group = {"patient_cpf": cpf_get_function(data), "data": data}
+        try:
+            patient_cpf = cpf_get_function(data)
+        except TypeError as e:
+            logger.warning(f"Skipping data item: {data}. Reason: {e}")
+            continue
+
+        group = {"patient_cpf": patient_cpf, "data": data}
         groups.append(group)
-    return groups
