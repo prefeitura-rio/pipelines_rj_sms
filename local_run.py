84df02f4aa6d76057e2ce19a8ff218a5a893d334
Vitoria Leite <41980417+vitmesquita@users.noreply.github.com>
2024-03-12 11:54:16 -0300

    Update local_run.py

--- local_run.py
+++ local_run.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
 import argparse
-
-from pipelines.prontuarios.raw.smsrio.flows import sms_prontuarios_raw_smsrio
-from pipelines.prontuarios.raw.vitacare.flows import vitacare_extraction
-from pipelines.prontuarios.raw.vitai.flows import vitai_extraction
-from pipelines.prontuarios.std.smsrio.flows import smsrio_standardization
 from pipelines.prontuarios.std.vitai.flows import vitai_standardization
 
 # ==================================================
