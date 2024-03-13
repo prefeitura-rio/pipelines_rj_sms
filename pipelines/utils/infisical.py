a25fa155eb0cfa8efb088702f5d17e073f59d2e9
Pedro Nascimento <pedrovitor.marques@gmail.com>
2024-03-13 14:21:18 -0300

    Skip Credential Setup if already made

--- pipelines/utils/infisical.py
+++ pipelines/utils/infisical.py
@@ -12,7 +12,7 @@ from prefeitura_rio.pipelines_utils.infisical import get_infisical_client, injec
 from prefeitura_rio.pipelines_utils.logging import log
 
 
-def inject_bd_credentials(environment: str = "dev") -> None:
+def inject_bd_credentials(environment: str = "dev", force_injection=False) -> None:
     """
     Loads Base dos Dados credentials from Infisical into environment variables.
 
@@ -22,6 +22,24 @@ def inject_bd_credentials(environment: str = "dev") -> None:
     Returns:
         None
     """
+    # Verify if all environment variables are already set
+    all_variables_set = True
+    for variable in [
+        "BASEDOSDADOS_CONFIG", 
+        "BASEDOSDADOS_CREDENTIALS_PROD", 
+        "BASEDOSDADOS_CREDENTIALS_STAGING",
+        "GOOGLE_APPLICATION_CREDENTIALS"
+        ]:
+        if not os.environ.get(variable):
+            all_variables_set = False
+            break
+
+    # If all variables are set, skip injection
+    if all_variables_set and not force_injection:
+        log("All environment variables are already set. Skipping injection.")
+        return
+
+    # Else inject the variables
     client = get_infisical_client()
 
     log(f"ENVIROMENT: {environment}")
@@ -46,3 +64,4 @@ def inject_bd_credentials(environment: str = "dev") -> None:
     with open("/tmp/credentials.json", "wb") as credentials_file:
         credentials_file.write(service_account)
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/tmp/credentials.json"
+
