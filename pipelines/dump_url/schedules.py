d3e1f23f72f74e0f662e40c2cc3cddf539fac10b
ThiagoTrabach <thiago.trabach@gmail.com>
2024-03-13 15:11:43 -0300

    Update start date and runs interval in dump URL schedules

--- pipelines/dump_url/schedules.py
+++ pipelines/dump_url/schedules.py
@@ -38,12 +38,12 @@ flow_parameters = [
 
 clocks = generate_dump_api_schedules(
     interval=timedelta(days=1),
-    start_date=datetime(2023, 1, 1, 15, 0, tzinfo=pytz.timezone("America/Sao_Paulo")),
+    start_date=datetime(2023, 1, 1, 15, 18, tzinfo=pytz.timezone("America/Sao_Paulo")),
     labels=[
         constants.RJ_SMS_AGENT_LABEL.value,
     ],
     flow_run_parameters=flow_parameters,
-    runs_interval_minutes=0,
+    runs_interval_minutes=3,
 )
 
 daily_update_schedule = Schedule(clocks=untuple_clocks(clocks))
