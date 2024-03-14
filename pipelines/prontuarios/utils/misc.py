1fbcb99fd5e024296614cf787e24a9171347d039
Pedro Marques <pedrovitor.marques@gmail.com>
2024-03-14 15:20:03 -0300

    Improving Date Parsing

--- pipelines/prontuarios/utils/misc.py
+++ pipelines/prontuarios/utils/misc.py
@@ -12,6 +12,15 @@ def split_dataframe(df, chunk_size=10000):
         chunks.append(df[i * chunk_size : (i + 1) * chunk_size])  # noqa: E203
     return chunks
 
+def date_parser(date: str) -> str:
+    if "/" in date:
+        # Caso tipico Vitacare. Ex.: 01/12/2020
+        new_date = dateutil.parser.parse(date, dayfirst=True)
+    else:
+        # Caso tipico Vitai/SMSRio. Ex.: 2020-12-01
+        new_date = dateutil.parser.parse(date)
+    return new_date
+
 
 def build_additional_fields(
     data_list: list,
@@ -47,19 +56,19 @@ def build_additional_fields(
         clean_patient_cpf = re.sub(r"\D", "", patient_cpf)
 
         birth_date = birth_data_get_function(data)
-        birth_date = dateutil.parser.parse(birth_date, dayfirst=True)
-        birth_date = birth_date.strftime("%Y%m%d")
+        birth_date = date_parser(birth_date)
 
-        patient_code = f"{clean_patient_cpf}.{birth_date}"
+        patient_code = f"""{clean_patient_cpf}.{birth_date.strftime("%Y%m%d")}"""
 
         # ------------------
         # Source Updated At
         # ------------------
         source_updated_at = source_updated_at_get_function(data)
         if source_updated_at is None:
-            source_updated_at = birth_data_get_function(data)
+            source_updated_at = birth_date
+        else:
+            source_updated_at = date_parser(source_updated_at)
 
-        source_updated_at = dateutil.parser.parse(source_updated_at)
         source_updated_at = source_updated_at.strftime("%Y-%m-%d %H:%M:%S")
 
         # ------------------
