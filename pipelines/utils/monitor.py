64bdaf248ecf06fc450f05a1b2f33e754a76e0ab
Pedro Marques <pedrovitor.marques@gmail.com>
2024-03-21 17:02:09 -0300

    Bugfix: secret getting

--- pipelines/utils/monitor.py
+++ pipelines/utils/monitor.py
@@ -8,7 +8,7 @@ from prefeitura_rio.pipelines_utils.infisical import get_secret
 
 def send_message(message, username=None):
     async def send_message(message, username=None):
-        webhook_url = get_secret(secret_name="DISCORD_WEBHOOK_URL", environment="prod")
+        webhook_url = get_secret(secret_name="DISCORD_WEBHOOK_URL", environment="prod").get('DISCORD_WEBHOOK_URL')
         async with aiohttp.ClientSession() as session:
             webhook = Webhook.from_url(webhook_url, session=session)
             if username:
