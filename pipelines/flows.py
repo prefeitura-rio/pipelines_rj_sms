2695b37bd6fa06112a5710309f8d7505e1a28167
Pedro Nascimento <pedrovitor.marques@gmail.com>
2024-03-22 13:43:33 -0300

    Add endpoint health report flow

--- pipelines/flows.py
+++ pipelines/flows.py
@@ -20,3 +20,4 @@ from pipelines.prontuarios.std.smsrio.flows import *
 from pipelines.prontuarios.std.vitai.flows import *
 from pipelines.tools.api_healthcheck.flows import *
 from pipelines.tools.unschedule_old_flows.flows import *
+from pipelines.reports.endpoint_health.flows import *
