4de4d5a8522297e5c2e38d6492096529725f0547
vitmesquita <vitoria.leite.ismart@gmail.com>
2024-03-22 12:03:06 -0300

    deleting extracao files

--- pipelines/flows.py
+++ pipelines/flows.py
@@ -15,7 +15,6 @@ from pipelines.execute_dbt.flows import *
 from pipelines.prontuarios.raw.smsrio.flows import *
 from pipelines.prontuarios.raw.vitacare.flows import *
 from pipelines.prontuarios.raw.vitai.flows import *
-from pipelines.prontuarios.std.extracao.flows import *
 from pipelines.prontuarios.std.smsrio.flows import *
 from pipelines.prontuarios.std.vitai.flows import *
 from pipelines.tools.api_healthcheck.flows import *
