49479c4ff15d177c30c3aca89c371a7316bb270d
Pedro Nascimento <pedrovitor.marques@gmail.com>
2024-03-22 13:02:33 -0300

    Creating API Report Flow

---
+++ pipelines/reports/endpoint_health/flows.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from prefect import Parameter
+from prefect.executors import LocalDaskExecutor
+from prefect.run_configs import KubernetesRun
+from prefect.storage import GCS
+from prefeitura_rio.pipelines_utils.custom import Flow
+
+from pipelines.constants import constants
+from pipelines.reports.endpoint_health.tasks import (
+    create_description,
+    send_report
+)
+from pipelines.utils.tasks import load_file_from_bigquery
+
+with Flow(
+    name="Report: Disponibilidade de API",
+) as disponibilidade_api:
+    ENVIRONMENT = Parameter("environment", default="dev")
+
+    endpoints_table = load_file_from_bigquery(
+        project_name="rj-sms",
+        dataset_name="gerenciamento",
+        table_name="api_url_list",
+        environment=ENVIRONMENT,
+    )
+    health_check_results_table = load_file_from_bigquery(
+        project_name="rj-sms",
+        dataset_name="gerenciamento",
+        table_name="api_health_check",
+        environment=ENVIRONMENT,
+    )
+    text_report = create_description(
+        endpoints_table=endpoints_table,
+        results_table=health_check_results_table
+    )
+    send_report(plot=None, description=text_report)
+
+disponibilidade_api.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
+disponibilidade_api.executor = LocalDaskExecutor(num_workers=1)
+disponibilidade_api.run_config = KubernetesRun(
+    image=constants.DOCKER_IMAGE.value,
+    labels=[
+        constants.RJ_SMS_AGENT_LABEL.value,
+    ],
+    memory_request="2Gi",
+    memory_limit="2Gi",
+)
---
+++ pipelines/reports/endpoint_health/tasks.py
@@ -0,0 +1,58 @@
+import datetime
+import numpy as np
+import pandas as pd
+
+import pipelines.utils.monitor as monitor
+from pipelines.utils.credential_injector import authenticated_task as task
+
+
+@task
+def create_description(endpoints_table, results_table):
+    metrics = []
+    for _, endpoint in endpoints_table.iterrows():
+        records = results_table[results_table["api_url"] == endpoint["url"]]
+        records.sort_values(by="moment", inplace=True)
+
+        last_healthy_record = records[records["is_healthy"] == True].tail(1)['moment'].values[0]
+        last_healthy_record = last_healthy_record.item().strftime("%d/%m/%Y %H:%M:%S")
+
+        yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+        records_today = records[pd.to_datetime(
+            records.moment).dt.tz_localize(None) > np.datetime64(yesterday)]
+
+        availability = records_today["is_healthy"].mean()
+
+        metrics.append(
+            (endpoint['title'], availability, last_healthy_record)
+        )
+
+    metrics.sort(key=lambda x: x[1], reverse=False)
+
+    reports = []
+    for name, availability, last_healthy in metrics:
+        status_emoji = "❌"
+        if availability == 1.0:
+            status_emoji = "✅"
+        elif availability >= 0.9:
+            status_emoji = "⚠️"
+
+        reports.append(
+            f"- {status_emoji} **{name.upper()}**: {availability:.0%} de disponibilidade.\n   - Última vez disponível em: {last_healthy}" #noqa
+        )
+    report = "\n".join(reports)
+    return report
+
+
+@task
+def create_plot(endpoints_table, results_table):
+    return None
+
+
+@task
+def send_report(plot, description):
+
+    monitor.send_message(
+        title="Disponibilidade de API nas últimas 24h",
+        message=description
+    )
+    return
