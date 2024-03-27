3c34681d6f65c852260ee0c087be875139accb9b
ThiagoTrabach <thiago.trabach@gmail.com>
2024-03-27 11:14:52 -0300

    Update start date for dump_api_schedules

--- pipelines/dump_api_vitacare/schedules.py
+++ pipelines/dump_api_vitacare/schedules.py
@@ -51,7 +51,7 @@ update_parameters = posicao_parameters + movimento_parameters
 
 update_clocks = generate_dump_api_schedules(
     interval=timedelta(days=1),
-    start_date=datetime(2023, 1, 1, 10, 50, tzinfo=pytz.timezone("America/Sao_Paulo")),
+    start_date=datetime(2023, 1, 1, 12, 0, tzinfo=pytz.timezone("America/Sao_Paulo")),
     labels=[
         constants.RJ_SMS_AGENT_LABEL.value,
     ],
