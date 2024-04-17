ee1dbc5ae6ee484959876ca7f6bdd695e60103aa
Pedro Nascimento <pedrovitor.marques@gmail.com>
2024-04-17 09:07:58 -0300

    Increasing Memory Request/Limit in Vitacare Extraction

--- pipelines/prontuarios/raw/vitacare/flows.py
+++ pipelines/prontuarios/raw/vitacare/flows.py
@@ -131,8 +131,8 @@ vitacare_extraction.run_config = KubernetesRun(
     labels=[
         constants.RJ_SMS_AGENT_LABEL.value,
     ],
-    memory_request="8Gi",
-    memory_limit="8Gi",
+    memory_request="12Gi",
+    memory_limit="12Gi",
 )
 
 
