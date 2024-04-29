82287989b3b44c57577d858766d6f87c3725ba51
Pedro Nascimento <pedrovitor.marques@gmail.com>
2024-04-29 16:05:26 -0300

    Implementing Vitacare Healthcheck as Flow

---
+++ pipelines/tools/vitacare_healthcheck/constants.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=C0103
+"""
+Constants
+"""
+from enum import Enum
+
+
+class constants(Enum):
+    TARGET_FOLDER_ID = '1ClVX2dbWNxg2yHtXyH9mtEICrUpsy8bN'
+
+    PROJECT_NAME = {
+        'prod': 'rj-sms-prod',
+        'staging': 'rj-sms-dev',
+        'dev': 'rj-sms-dev',
+    }
+    DATASET_NAME = 'gerenciamento__monitoramento_vitacare'
+    TABLE_NAME = 'result'
