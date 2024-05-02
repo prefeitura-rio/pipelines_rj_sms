7eab8211b130290e66c262b8de7ebb885f975519
Pedro Nascimento <pedrovitor.marques@gmail.com>
2024-05-02 18:18:35 -0300

    Refactor flows.py to remove unused imports

--- pipelines/tools/vitacare_healthcheck/flows.py
+++ pipelines/tools/vitacare_healthcheck/flows.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-from prefect import Parameter, case
-from prefect.tasks.control_flow import merge
+from prefect import Parameter
 from prefect.executors import LocalDaskExecutor
 from prefect.run_configs import KubernetesRun
 from prefect.storage import GCS
