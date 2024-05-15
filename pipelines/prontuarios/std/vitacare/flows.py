2d0891ac64d5057c6fd8d0549f5d781ddd0407f2
vitmesquita <vitoria.leite.ismart@gmail.com>
2024-05-15 09:52:19 -0300

    add parameters to schedule run

---
+++ pipelines/prontuarios/std/vitacare/flows.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+from prefect import Parameter, case, unmapped
+from prefect.executors import LocalDaskExecutor
+from prefect.run_configs import KubernetesRun
+from prefect.storage import GCS
+from prefeitura_rio.pipelines_utils.custom import Flow
+
+from pipelines.constants import constants
+from pipelines.prontuarios.constants import constants as prontuarios_constants
+from pipelines.prontuarios.std.vitacare.constants import constants as vitacare_constants
+from pipelines.prontuarios.std.vitacare.schedules import vitacare_std_daily_update_schedule
+from pipelines.prontuarios.std.vitacare.tasks import (
+    define_constants,
+    format_json,
+    get_params,
+    standartize_data,
+)
+from pipelines.prontuarios.utils.tasks import (
+    get_api_token,
+    get_std_flow_scheduled_day,
+    load_to_api,
+    rename_current_std_flow_run,
+    transform_create_input_batches,
+)
+from pipelines.utils.tasks import get_secret_key, load_from_api
+
+with Flow(
+    name="Prontuários (Vitacare) - Padronização de Pacientes",
+) as vitacare_standardization:
+    #####################################
+    # Parameters
+    #####################################
+    ENVIRONMENT = Parameter("environment", default="dev", required=True)
+    RENAME_FLOW = Parameter("rename_flow", default=False)
+
+    ####################################
+    # Set environment
+    ####################################
+
+    with case(RENAME_FLOW, True):
+        rename_flow_task = rename_current_std_flow_run(environment=ENVIRONMENT, unidade="VITACARE")
+
+    api_token = get_api_token(
+        environment=ENVIRONMENT,
+        infisical_path=vitacare_constants.INFISICAL_PATH.value,
+        infisical_api_url=prontuarios_constants.INFISICAL_API_URL.value,
+        infisical_api_username=vitacare_constants.INFISICAL_API_USERNAME.value,
+        infisical_api_password=vitacare_constants.INFISICAL_API_PASSWORD.value,
+    )
+
+    api_url = get_secret_key(
+        secret_path="/",
+        secret_name=prontuarios_constants.INFISICAL_API_URL.value,
+        environment=ENVIRONMENT,
+    )
+
+    ####################################
+    # Task Section #1 - Get Data
+    ####################################
+    START_DATETIME = get_std_flow_scheduled_day()
+    request_params = get_params(start_datetime=START_DATETIME)
+
+    raw_patient_data = load_from_api(
+        url=api_url + "raw/patientrecords/fromInsertionDatetime",
+        params=request_params,
+        credentials=api_token,
+        auth_method="bearer",
+    )
+
+    ####################################
+    # Task Section #2 - Transform Data
+    ####################################
+
+    lista_campos_api, logradouros_dict, city_dict, state_dict, country_dict = define_constants()
+
+    json_list_valid, list_invalid_id = format_json(json_list=raw_patient_data)
+
+    load_to_api_invalid_ids = load_to_api(
+        request_body=list_invalid_id,
+        endpoint_name="raw/patientrecords/setAsInvalid",
+        api_token=api_token,
+        environment=ENVIRONMENT,
+    )
+
+    std_patient_list = standartize_data(
+        raw_data=json_list_valid,
+        logradouros_dict=logradouros_dict,
+        city_dict=city_dict,
+        state_dict=state_dict,
+        country_dict=country_dict,
+        lista_campos_api=lista_campos_api,
+    )
+
+    std_patient_list_batches = transform_create_input_batches(
+        input_list=std_patient_list, batch_size=1000
+    )
+
+    load_to_api_task = load_to_api.map(
+        request_body=std_patient_list_batches,
+        endpoint_name=unmapped("std/patientrecords"),
+        api_token=unmapped(api_token),
+        environment=unmapped(ENVIRONMENT),
+    )
+
+
+vitacare_standardization.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
+vitacare_standardization.executor = LocalDaskExecutor(num_workers=4)
+vitacare_standardization.run_config = KubernetesRun(
+    image=constants.DOCKER_IMAGE.value,
+    labels=[
+        constants.RJ_SMS_AGENT_LABEL.value,
+    ],
+    memory_limit="5Gi",
+)
+
+vitacare_standardization.schedule = vitacare_std_daily_update_schedule
---
+++ pipelines/prontuarios/std/vitacare/tasks.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+from datetime import timedelta
+from typing import Tuple
+
+import pandas as pd
+from prefeitura_rio.pipelines_utils.logging import log
+
+from pipelines.prontuarios.std.formatters.generic.patient import (
+    clean_none_records,
+    drop_invalid_records,
+    merge_keys,
+    prepare_to_load,
+)
+from pipelines.prontuarios.std.formatters.vitacare.patient import (
+    standardize_address_data,
+    standardize_cns_list,
+    standardize_decease_info,
+    standardize_nationality,
+    standardize_parents_names,
+    standardize_race,
+    standardize_telecom_data,
+)
+from pipelines.utils.credential_injector import authenticated_task as task
+
+
+@task
+def get_params(start_datetime: str) -> dict:
+    """
+    Creating params
+    Args:
+        start_datetime (str) : initial date extraction.
+
+    Returns:
+        dict : params dictionary
+    """
+    end_datetime = start_datetime + timedelta(days=1)
+    log(
+        f"""
+        Standardizing from {start_datetime.strftime("%Y-%m-%d 00:00:00")}
+        to {end_datetime.strftime("%Y-%m-%d 00:00:00")}"""
+    )
+    return {
+        "start_datetime": start_datetime.strftime("%Y-%m-%d 00:00:00"),
+        "end_datetime": end_datetime.strftime("%Y-%m-%d 00:00:00"),
+        "datasource_system": "vitacare",
+    }
+
+
+@task(nout=2)
+def define_constants() -> Tuple[dict, dict, dict]:
+    """
+    Creating constants as global variables
+
+    Returns:
+        lista_campos_api (list) : list of API acceptable parameters
+        logradouros_dict (dict) : From/to dictionary to normalize logradouros
+        city_dict (dict) : From/to dictionary to normalize city
+        state_dict (dict) : From/to dictionary to normalize state
+        country_dict (dict) : From/to dictionary to normalize country
+    """
+
+    city = pd.read_csv(
+        "/home/jupyter/data/municipios.csv", dtype={"COD UF": str, "COD": str, "NOME": str}
+    )
+    city_dict = dict(zip(city["COD"].str.slice(start=0, stop=-1), city["COD"]))
+
+    countries = pd.read_csv(
+        "/home/jupyter/data/paises.csv", dtype={"country": str, "code": str}
+    )
+    country_dict = dict(zip(countries["country"].str.upper(), countries["code"]))
+
+    return city_dict, country_dict
+
+
+@task(nout=2)
+def format_json(json_list: list) -> list:
+    """
+    Drop invalid patient records, i.e. records without valid inputs on required fields
+    Args:
+        json_list (list): Payload from raw API
+    Returns:
+        json_list_notna (list): Valid patient inputs ready to standardize
+    """
+
+    print(f"Starting flow with {len(json_list)} registers")
+
+    json_list_merged = list(map(merge_keys, json_list))
+    json_list_info = list(map(drop_invalid_records, json_list_merged))
+    json_list_valid, json_list_invalid = clean_none_records(json_list_info)
+
+    print(
+        f"{len(json_list_invalid)} registers dropped,\
+        standardizing remaining {len(json_list_valid)} registers"
+    )
+
+    return json_list_valid, json_list_invalid
+
+
+@task
+def standartize_data(
+    raw_data: list, city_dict: dict, country_dict: dict
+) -> list:
+    """
+    Standardize fields and prepare to post
+    Args:
+        raw_data (list): Raw data to be standardized
+        city_name_dict (dict): From/to dictionary to code city names
+        state_dict (dict): From/to dictionary to code state names
+        country_dict (dict): From/to dictionary to code country names
+    Returns:
+        list: Data ready to load to API
+    """
+
+    print("Starting standardization of race field")
+    patients_json_std_race = list(map(standardize_race, raw_data))
+
+    print("Starting standardization of nationality field")
+    patients_json_std_nationality = list(map(standardize_nationality, patients_json_std_race))
+
+    print("Starting standardization of parents name field")
+    patients_json_std_parents = list(map(standardize_parents_names, patients_json_std_nationality))
+
+    print("Starting standardization of cns field")
+    patients_json_std_cns = list(map(standardize_cns_list, patients_json_std_parents))
+
+    print("Starting standardization of decease field")
+    patients_json_std_decease = list(map(standardize_decease_info, patients_json_std_cns))
+
+    print("Starting standardization of address field")
+    patients_json_std_address = list(
+        map(
+            lambda p: standardize_address_data(
+                data=p,
+                city_dict=city_dict,
+                country_dict=country_dict,
+            ),
+            patients_json_std_decease,
+        )
+    )
+
+    print("Starting standardization of telecom field")
+    patients_json_std_telecom = list(map(standardize_telecom_data, patients_json_std_address))
+
+    print("Preparing data to load")
+    patients_json_std_clean = list(map(lambda x: prepare_to_load(x), patients_json_std_telecom))
+
+    return patients_json_std_clean
