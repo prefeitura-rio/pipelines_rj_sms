95da81eefd18c6dfd8fad19eb85fcf7f40991a1c
ThiagoTrabach <thiago.trabach@gmail.com>
2024-05-23 13:57:58 -0300

    chore: Update machine type for sms_dump_datasus in datasus_ftp/flows.py

--- pipelines/datalake/extract_load/datasus_ftp/flows.py
+++ pipelines/datalake/extract_load/datasus_ftp/flows.py
@@ -101,7 +101,7 @@ sms_dump_datasus.run_config = VertexRun(
         constants.RJ_SMS_VERTEX_AGENT_LABEL.value,
     ],
     # https://cloud.google.com/vertex-ai/docs/training/configure-compute#machine-types
-    machine_type="e2-standard-2",
+    machine_type="e2-standard-4",
     env={
         "INFISICAL_ADDRESS": constants.INFISICAL_ADDRESS.value,
         "INFISICAL_TOKEN": constants.INFISICAL_TOKEN.value,
