de68bf3953f3ded1b300674db884d9753976dd65
ThiagoTrabach <thiago.trabach@gmail.com>
2024-05-23 14:24:07 -0300

    chore: Update start time and runs interval for dump API schedules in datasus_ftp/schedules.py

--- pipelines/datalake/extract_load/datasus_ftp/schedules.py
+++ pipelines/datalake/extract_load/datasus_ftp/schedules.py
@@ -16,14 +16,14 @@ flow_parameters = [
     {
         "dataset_id": "brutos_cnes_web",
         "endpoint": "cnes",
-        "environment": "prod",
+        "environment": "dev",
         "download_newest": True,
         "rename_flow": True,
     },
     {
         "dataset_id": "brutos_datasus",
         "endpoint": "cbo",
-        "environment": "prod",
+        "environment": "dev",
         "download_newest": False,
         "file": "TAB_CNES.zip",
         "rename_flow": True,
@@ -32,12 +32,12 @@ flow_parameters = [
 
 clocks = generate_dump_api_schedules(
     interval=timedelta(days=7),
-    start_date=datetime(2024, 5, 26, 8, 0, tzinfo=pytz.timezone("America/Sao_Paulo")),
+    start_date=datetime(2024, 5, 23, 14, 35, tzinfo=pytz.timezone("America/Sao_Paulo")),
     labels=[
         constants.RJ_SMS_VERTEX_AGENT_LABEL.value,
     ],
     flow_run_parameters=flow_parameters,
-    runs_interval_minutes=60,
+    runs_interval_minutes=30,
 )
 
 datasus_weekly_update_schedule = Schedule(clocks=untuple_clocks(clocks))
