5299b69552260808d07be1bc3ee290ced0c0f5fd
vitmesquita <vitoria.leite.ismart@gmail.com>
2024-06-13 16:01:17 -0300

    creating flow

---
+++ pipelines/datalake/extract_load/historico_clinico_integrado/constants.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=C0103
+"""
+Constants for SMSRio.
+"""
+from enum import Enum
+
+
+class constants(Enum):
+    """
+    Constant values for the dump hci flow
+    """
+
+    INFISICAL_PATH = "/"
+    INFISICAL_DB_URL = "PRONTUARIOS_DB_URL"
+    SCHEMA_ID = ""
+    DATASET_ID = "historico_clinico_integrado"
+    TABLE_ID = {"patient":"patient_records"}
