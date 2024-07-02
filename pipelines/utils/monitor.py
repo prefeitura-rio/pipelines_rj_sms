2d78fee3a53e4224a9fd7a429c183f6d8abe5cf6
Pedro Nascimento <pedrovitor.marques@gmail.com>
2024-07-02 11:30:49 -0300

    fix: handle long message content in Discord Monitor

--- pipelines/utils/monitor.py
+++ pipelines/utils/monitor.py
@@ -29,6 +29,9 @@ async def send_discord_webhook(
     secret_name = f"DISCORD_WEBHOOK_URL_{monitor_slug.upper()}"
     webhook_url = get_secret(secret_name=secret_name, environment=environment).get(secret_name)
 
+    if len(text_content)> 2000:
+        raise ValueError(f"Message content is too long: {len(text_content)} > 2000 characters.")
+
     async with aiohttp.ClientSession() as session:
         kwargs = {"content": text_content}
         if username:
