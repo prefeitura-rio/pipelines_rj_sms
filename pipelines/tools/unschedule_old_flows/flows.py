5f1e20718be844778d10a4cdcb3805d3b9a526e1
Pedro Nascimento <pedrovitor.marques@gmail.com>
2024-07-02 09:30:51 -0300

    fix: wrong param name in task

--- pipelines/tools/unschedule_old_flows/flows.py
+++ pipelines/tools/unschedule_old_flows/flows.py
@@ -17,7 +17,7 @@ with Flow("Tool: Desagendador de Flows Fantasmas") as unscheduler_flow:
     non_archived_flows = query_non_archived_flows(environment=ENVIRONMENT)
 
     archived_flow_runs = query_archived_flow_versions_with_runs.map(
-        flows=non_archived_flows,
+        flow_data=non_archived_flows,
         environment=ENVIRONMENT
     )
 
