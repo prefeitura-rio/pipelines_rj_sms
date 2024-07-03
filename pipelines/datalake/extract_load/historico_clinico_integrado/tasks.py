d9ecee33514c85b21a544464d10696b11a51573d
vitmesquita <vitoria.leite.ismart@gmail.com>
2024-07-03 16:01:41 -0300

    fix file path

--- pipelines/datalake/extract_load/historico_clinico_integrado/tasks.py
+++ pipelines/datalake/extract_load/historico_clinico_integrado/tasks.py
@@ -51,7 +51,7 @@ def download_from_db(
 
     log(f"{len(table)} rows downloaded")
 
-    destination_file_path = f"{file_folder}/{file_name}.csv"
+    destination_file_path = f"{file_folder}/{file_name}/{prefect.context.get("scheduled_start_time").date()}.csv"
 
     table.to_csv(destination_file_path, index=False, sep=";", encoding="utf-8")
 
