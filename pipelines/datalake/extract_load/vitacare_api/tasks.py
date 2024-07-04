9fe8c4175429566c5531104f5638bd7ff1b0baba
ThiagoTrabach <thiago.trabach@gmail.com>
2024-07-04 07:50:39 -0300

    chore: Increase max retries for API data extraction task

--- pipelines/datalake/extract_load/vitacare_api/tasks.py
+++ pipelines/datalake/extract_load/vitacare_api/tasks.py
@@ -34,7 +34,7 @@ from pipelines.utils.tasks import (
 )
 
 
-@task(max_retries=2, retry_delay=timedelta(minutes=4))
+@task(max_retries=3, retry_delay=timedelta(minutes=4))
 def extract_data_from_api(
     cnes: str, ap: str, target_day: str, endpoint: str, environment: str = "dev"
 ) -> dict:
