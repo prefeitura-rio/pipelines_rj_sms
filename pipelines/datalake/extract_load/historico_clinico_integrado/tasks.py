b3ec69dd94bc37e2a96a81b7ed915584c41bd826
vitmesquita <vitoria.leite.ismart@gmail.com>
2024-07-12 15:41:43 -0300

    assign variable window_date

--- pipelines/datalake/extract_load/historico_clinico_integrado/tasks.py
+++ pipelines/datalake/extract_load/historico_clinico_integrado/tasks.py
@@ -42,7 +42,7 @@ def download_from_db(
     connection_string = f"{db_url}"
     if (historical_mode is False) & (target_date == ""):
         target_date = prefect.context.get("scheduled_start_time").date()
-    window_date = target_date - timedelta(days=7)
+    window_date = pd.to_datetime(target_date).date()- timedelta(days=7)
     time_clause = (
         f"""
             WHERE
