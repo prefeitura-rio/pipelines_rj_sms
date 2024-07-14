3b5d9a5678f344e5c7600ca425a53f9560ad7c63
ThiagoTrabach <thiago.trabach@gmail.com>
2024-07-14 10:23:45 -0300

    chore: Update start date for Seguir em Frente Pipefy flow schedules

--- pipelines/datalake/extract_load/seguir_em_frente_pipefy/schedules.py
+++ pipelines/datalake/extract_load/seguir_em_frente_pipefy/schedules.py
@@ -38,7 +38,7 @@ flow_parameters = [
 
 clocks = generate_dump_api_schedules(
     interval=timedelta(days=1),
-    start_date=datetime(2024, 1, 1, 9, 10, tzinfo=pytz.timezone("America/Sao_Paulo")),
+    start_date=datetime(2024, 1, 1, 0, 45, tzinfo=pytz.timezone("America/Sao_Paulo")),
     labels=[
         constants.RJ_SMS_AGENT_LABEL.value,
     ],
