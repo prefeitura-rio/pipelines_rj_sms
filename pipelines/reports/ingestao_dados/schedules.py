c4f0e53777fa5a31bcab2eacbc0e80bf4485bdb7
Pedro Marques <pedrovitor.marques@gmail.com>
2024-10-17 16:47:12 -0300

    chore: Add weekly schedule for report_monitoramento_ingestao

---
+++ pipelines/reports/ingestao_dados/schedules.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=C0103
+"""
+Schedules
+"""
+
+from datetime import datetime, timedelta
+
+import pytz
+from prefect.schedules import Schedule
+
+from pipelines.constants import constants
+from pipelines.utils.schedules import generate_dump_api_schedules, untuple_clocks
+
+flow_parameters = [
+    {
+        "target_date": None,
+        "environment": "prod"
+    },
+]
+
+
+clocks = generate_dump_api_schedules(
+    interval=timedelta(days=1),
+    start_date=datetime(2024, 9, 23, 7, 0, tzinfo=pytz.timezone("America/Sao_Paulo")),
+    labels=[
+        constants.RJ_SMS_AGENT_LABEL.value,
+    ],
+    flow_run_parameters=flow_parameters,
+    runs_interval_minutes=0,
+)
+
+weekly_schedule = Schedule(clocks=untuple_clocks(clocks))
