49f186ded2c493c14c04483f3a2330f62b0012d1
ThiagoTrabach <thiago.trabach@gmail.com>
2024-10-23 09:01:15 -0300

    wip

--- pipelines/datalake/extract_load/vitacare_api/tasks.py
+++ pipelines/datalake/extract_load/vitacare_api/tasks.py
@@ -100,16 +100,8 @@ def extract_data_from_api(
                 requested_data[0]["dtaReplicacao"], "%Y-%m-%d %H:%M:%S.%f"
             ).date()
 
-            # yesterday_cutoff = (date.today() - timedelta(days=1)).replace(
-            #     hour=22, minute=0, second=0
-            # )
             if replication_date != date.today():
-                # if replication_date < yesterday_cutoff:
-                err_msg = (
-                    f"API data is outdated. "
-                    f"Last update at API: {replication_date}, "
-                    # f"Expected update after: {yesterday_cutoff}. "
-                )
+                err_msg = f"Date mismatch: replication date is {replication_date} instead of {date.today()}"  # noqa: E501
                 logger.error(err_msg)
                 return {"has_data": False}
 
