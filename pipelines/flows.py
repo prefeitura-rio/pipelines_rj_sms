e622ec0fe753abf8bf04b54c14ec08166892ddd5
Pedro Marques <pedrovitor.marques@gmail.com>
2024-10-23 13:12:20 -0300

    chore: Add datalake_bigquery flow to flows.py

--- pipelines/flows.py
+++ pipelines/flows.py
@@ -18,6 +18,7 @@ from pipelines.datalake.extract_load.vitacare_api.flows import *
 from pipelines.datalake.extract_load.vitacare_db.flows import *
 from pipelines.datalake.extract_load.vitai_api.flows import *
 from pipelines.datalake.extract_load.vitai_db.flows import *
+from pipelines.datalake.extract_load.datalake_bigquery.flows import *
 from pipelines.datalake.transform.dbt.flows import *
 from pipelines.prontuarios.load_datalake.flows import *
 from pipelines.prontuarios.raw.smsrio.flows import *
