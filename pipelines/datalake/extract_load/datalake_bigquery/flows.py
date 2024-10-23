8147a8f39cfaa529f8cd440a0205f78b97565111
Pedro Marques <pedrovitor.marques@gmail.com>
2024-10-23 12:23:38 -0300

    feat: flow functionality

--- pipelines/datalake/extract_load/datalake_bigquery/flows.py
+++ pipelines/datalake/extract_load/datalake_bigquery/flows.py
@@ -11,6 +11,17 @@ from prefect.storage import GCS
 from prefeitura_rio.pipelines_utils.custom import Flow
 
 from pipelines.constants import constants
+from pipelines.utils.basics import is_null_or_empty
+from pipelines.utils.tasks import (
+    rename_current_flow_run,
+    get_bigquery_project_from_environment,
+    get_project_name,
+)
+from pipelines.utils.prefect import get_current_flow_labels
+from pipelines.utils.credential_injector import (
+    authenticated_create_flow_run as create_flow_run,
+    authenticated_wait_for_flow_run as wait_for_flow_run,
+)
 from pipelines.datalake.extract_load.datalake_bigquery.tasks import clone_bigquery_table
 
 
@@ -21,14 +32,40 @@ with Flow(
     TABLE_ID = Parameter("table_id", default=False)
     DBT_SELECT_EXP = Parameter("dbt_select_exp", default=None)
 
-    bigquery_project = get_bigquery_project_name(
+    bigquery_project = get_bigquery_project_from_environment(
         environment=ENVIRONMENT
     )
 
-    clone_table = clone_bigquery_table(
+    rename_current_flow_run(
+        name_template="Cloning table {table_id} from {bigquery_project}",
+        table_id=TABLE_ID,
+        bigquery_project=bigquery_project,
+    )
+
+    clone_table_task = clone_bigquery_table(
         table_id=TABLE_ID,
         project_name=bigquery_project,
     )
+
+    with case(is_null_or_empty(DBT_SELECT_EXP), False):
+        current_flow_run_labels = get_current_flow_labels()
+        project_name = get_project_name(environment=ENVIRONMENT)
+
+        dbt_run_flow = create_flow_run(
+            flow_name="DataLake - Transformação - DBT",
+            project_name=project_name,
+            parameters={
+                "command": "run",
+                "select": DBT_SELECT_EXP,
+                "environment": ENVIRONMENT,
+                "rename_flow": True,
+                "send_discord_report": False,
+            },
+            labels=current_flow_run_labels,
+            upstream_tasks=[clone_table_task],
+        )
+        wait_for_flow_run(flow_run_id=dbt_run_flow)
+
 
 datalake_bigquery_clone.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
 datalake_bigquery_clone.executor = LocalDaskExecutor(num_workers=10)
--- pipelines/datalake/extract_load/datalake_bigquery/tasks.py
+++ pipelines/datalake/extract_load/datalake_bigquery/tasks.py
@@ -1,6 +1,24 @@
-from pipelines.utils.credential_injector import authenticated_task as task
+from google.cloud import bigquery
 
+from pipelines.utils.credential_injector import authenticated_task as task
+from pipelines.utils.logger import log
 
 @task
-def clone_bigquery_table(table_id, project_name):
-    return
+def clone_bigquery_table(
+    source_table_id, 
+    destination_table_name, 
+    destination_dataset_name,
+    destination_project_name
+):
+    bq_client = bigquery.Client.from_service_account_json("/tmp/credentials.json")
+
+    destination_table_id = f"{destination_project_name}.{destination_dataset_name}.{destination_table_name}" # noqa
+
+    command = f"CREATE OR REPLACE TABLE `{destination_table_id}` CLONE `{source_table_id}`"
+    log(f"Running: {command}")
+
+    query_job = bq_client.query_and_wait(command)
+    result = query_job.result()
+    log(f"Query result: {result}")
+
+    return result
