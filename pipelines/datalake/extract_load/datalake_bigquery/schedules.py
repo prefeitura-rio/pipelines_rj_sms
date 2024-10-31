d757cd7f8d5d5d64027b68fec8ae35d42b05ab29
ThiagoTrabach <thiago.trabach@gmail.com>
2024-10-31 20:19:28 -0300

    fix create table handling if it already exists

--- pipelines/datalake/extract_load/datalake_bigquery/schedules.py
+++ pipelines/datalake/extract_load/datalake_bigquery/schedules.py
@@ -199,7 +199,7 @@ flow_parameters = [
 
 clocks = generate_dump_api_schedules(
     interval=timedelta(days=1),
-    start_date=datetime(2023, 1, 1, 20, 14, tzinfo=pytz.timezone("America/Sao_Paulo")),
+    start_date=datetime(2023, 1, 1, 6, 0, tzinfo=pytz.timezone("America/Sao_Paulo")),
     labels=[
         constants.RJ_SMS_AGENT_LABEL.value,
     ],
--- pipelines/datalake/extract_load/datalake_bigquery/tasks.py
+++ pipelines/datalake/extract_load/datalake_bigquery/tasks.py
@@ -2,7 +2,7 @@
 import google
 import google.api_core
 from google.cloud import bigquery
-from prefect.engine.signals import ENDRUN, FAIL
+from prefect.engine.signals import ENDRUN
 
 from pipelines.utils.credential_injector import authenticated_task as task
 from pipelines.utils.logger import log
@@ -33,7 +33,7 @@ def clone_bigquery_table(
 
         log("Trying to clone table by copying data from source to destination")
 
-        command = f"CREATE TABLE `{destination_table_id}` AS SELECT * FROM `{source_table_id}`"
+        command = f"DROP TABLE IF EXISTS `{destination_table_id}`; CREATE TABLE `{destination_table_id}` AS SELECT * FROM `{source_table_id}`"
         log(f"Running: {command}")
         query_job = bq_client.query_and_wait(command)
         job = bq_client.get_job(query_job.job_id)
