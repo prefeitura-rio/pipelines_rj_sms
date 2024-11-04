a9225c6f90825b6d27cb44e8178d65a6bb669873
Pedro Marques <pedrovitor.marques@gmail.com>
2024-11-04 18:07:51 -0300

    WIP: implemented flow, but missing two tasks

---
+++ pipelines/datalake/extract_load/vitacare_conectividade_gcs/tasks.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import json
+import re
+from pipelines.utils.credential_injector import authenticated_task as task
+from pipelines.utils.logger import log
+
+
+@task()
+def handle_json_files_from_gcs(files):
+    rows = []
+
+    for file_metadata, file_content in files:
+        row = dict()
+        row['file_name'] = file_metadata.name
+        row['gcs_created_at'] = file_metadata.time_created
+        row['gcs_updated_at'] = file_metadata.updated
+        row['datalake_loaded_at'] = pd.Timestamp.now().tz_localize('America/Sao_Paulo')
+
+        cnes, date = file_metadata.name.split('-', 1)
+        row['host_cnes'] = cnes
+
+        naive_date = pd.to_datetime(date.replace('.json', ''), format="%Y-%m-%d-%Hh-%Mm")
+        row['host_created_at'] = naive_date.tz_localize('America/Sao_Paulo')
+        try:
+            content_as_json = json.loads(file_content)
+            row["content"] = json.dumps(content_as_json)
+        except json.JSONDecodeError:
+            row["content"] = str(file_content)
+
+        rows.append(row)
+    
+    dataframe = pd.DataFrame.from_records(rows)
+
+    return dataframe
