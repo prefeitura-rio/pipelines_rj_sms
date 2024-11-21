7c0e5b0779fe3769b0630fe1a17ed62fa2f4d4a6
Pedro Marques <pedrovitor.marques@gmail.com>
2024-11-21 11:08:49 -0300

    feat: Update labels in vitai_db flows and schedules

--- pipelines/datalake/extract_load/vitai_db/flows.py
+++ pipelines/datalake/extract_load/vitai_db/flows.py
@@ -219,7 +219,7 @@ datalake_extract_vitai_db_manager.executor = LocalDaskExecutor(num_workers=1)
 datalake_extract_vitai_db_manager.run_config = KubernetesRun(
     image=global_constants.DOCKER_IMAGE.value,
     labels=[
-        global_constants.RJ_SMS_AGENT_LABEL_NEW.value,
+        global_constants.RJ_SMS_AGENT_LABEL.value,
     ],
     memory_limit="2Gi",
 )
