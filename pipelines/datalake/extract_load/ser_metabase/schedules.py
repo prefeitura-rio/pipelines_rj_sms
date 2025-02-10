87b224ede73cdd2c20ea9e6099a773e5b56f0bea
miloskimatheus <miloskimatheus@gmail.com>
2025-02-10 17:01:59 -0300

    fix clocks

--- pipelines/datalake/extract_load/ser_metabase/schedules.py
+++ pipelines/datalake/extract_load/ser_metabase/schedules.py
@@ -28,12 +28,11 @@ for dataset_name, dataset_config in DATABASE_IDS.items():
 
 clocks = generate_dump_api_schedules(
     interval=timedelta(days=1),
-    start_date=datetime(2025, 1, 27, 0, 0, tzinfo=pytz.timezone("America/Sao_Paulo")),
+    start_date=datetime(2023, 1, 1, 0, 1, tzinfo=pytz.timezone("America/Sao_Paulo")),
     labels=[
         constants.RJ_SMS_AGENT_LABEL.value,
     ],
     flow_run_parameters=flow_parameters,
-    runs_interval_minutes=0,
+    runs_interval_minutes=3,
 )
-
 schedule = Schedule(clocks=untuple_clocks(clocks))
