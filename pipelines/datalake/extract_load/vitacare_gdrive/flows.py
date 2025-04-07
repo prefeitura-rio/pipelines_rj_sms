2559cbbe3fecc9c617441a5822cc35770681b10f
Pedro <pedrovitor.marques@gmail.com>
2025-04-07 14:09:57 -0300

    feat: enhance Vitacare GDrive flow by implementing schema validation and reporting for file uploads, and adjusting executor settings

--- pipelines/datalake/extract_load/vitacare_gdrive/flows.py
+++ pipelines/datalake/extract_load/vitacare_gdrive/flows.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=C0103
 # flake8: noqa E501
-from prefect import Parameter
+from prefect import Parameter, unmapped
 from prefect.executors import LocalDaskExecutor
 from prefect.run_configs import KubernetesRun
 from prefect.storage import GCS
@@ -11,9 +11,10 @@ from pipelines.constants import constants
 from pipelines.datalake.extract_load.vitacare_gdrive.schedules import schedule
 from pipelines.datalake.extract_load.vitacare_gdrive.tasks import (
     find_all_file_names_from_pattern,
-    join_csv_files,
+    get_most_recent_schema,
+    report_inadequency,
+    upload_consistent_files,
 )
-from pipelines.utils.tasks import upload_df_to_datalake
 
 with Flow(
     name="DataLake - Extração e Carga de Dados - Vitacare GDrive",
@@ -34,26 +35,28 @@ with Flow(
         environment=ENVIRONMENT,
     )
 
-    dataframe = join_csv_files(
-        file_names=file_names,
+    most_recent_schema = get_most_recent_schema(
+        file_pattern=FILE_PATTERN,
         environment=ENVIRONMENT,
     )
 
-    upload_df_to_datalake(
-        df=dataframe,
-        partition_column="_loaded_at",
-        dataset_id=DESIRED_DATASET_NAME,
-        table_id=DESIRED_TABLE_NAME,
-        source_format="parquet",
-        dump_mode="overwrite",
-        if_exists="replace",
+    reports = upload_consistent_files.map(
+        file_name=file_names,
+        expected_schema=unmapped(most_recent_schema),
+        environment=unmapped(ENVIRONMENT),
+        dataset_id=unmapped(DESIRED_DATASET_NAME),
+        table_id=unmapped(DESIRED_TABLE_NAME),
     )
 
+    report_inadequency(
+        file_pattern=FILE_PATTERN,
+        reports=reports,
+    )
 
 # Storage and run configs
 sms_dump_vitacare_reports.schedule = schedule
 sms_dump_vitacare_reports.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
-sms_dump_vitacare_reports.executor = LocalDaskExecutor(num_workers=10)
+sms_dump_vitacare_reports.executor = LocalDaskExecutor(num_workers=1)
 sms_dump_vitacare_reports.run_config = KubernetesRun(
     image=constants.DOCKER_IMAGE.value,
     labels=[
--- pipelines/datalake/extract_load/vitacare_gdrive/tasks.py
+++ pipelines/datalake/extract_load/vitacare_gdrive/tasks.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import fnmatch
-
+from datetime import timedelta
 import pandas as pd
 from google.cloud import storage
 
@@ -8,9 +8,10 @@ from pipelines.datalake.extract_load.vitacare_gdrive.constants import constants
 from pipelines.datalake.extract_load.vitacare_gdrive.utils import download_file
 from pipelines.utils.credential_injector import authenticated_task as task
 from pipelines.utils.logger import log
+from pipelines.utils.tasks import upload_df_to_datalake
 
 
-@task
+@task(max_retries=3, retry_delay=timedelta(seconds=30))
 def find_all_file_names_from_pattern(file_pattern: str, environment: str):
     client = storage.Client()
 
@@ -26,46 +27,105 @@ def find_all_file_names_from_pattern(file_pattern: str, environment: str):
             files.append(blob.name)
 
     log(f"{len(files)} files were found. Their names:\n - " + "\n - ".join(files))
-    return files
+    return files[:5]
 
-
-@task
-def join_csv_files(file_names: list[str], environment: str) -> pd.DataFrame:
+@task(max_retries=3, retry_delay=timedelta(seconds=30))
+def get_most_recent_schema(file_pattern: str, environment: str) -> pd.DataFrame:
     client = storage.Client()
     bucket_name = constants.GCS_BUCKET.value[environment]
     bucket = client.bucket(bucket_name)
 
-    log(f"Downloading {len(file_names)} files")
+    blobs = bucket.list_blobs()
+    files = []
+    for blob in blobs:
+        if fnmatch.fnmatch(blob.name, file_pattern):
+            files.append(blob)
+    log(f"{len(files)} files were found")
 
-    def verify_columns_consistency(df_columns: list, found_columns: list) -> bool:
-        consistency = len(set(found_columns).intersection(set(df_columns))) / len(df_columns)
-        log(f"Consistency: {consistency}")
-        return consistency >= 0.8
+    files.sort(key=lambda x: x.updated)
+    most_recent_file = files[-1].name
+    log(f"Most recent file: {most_recent_file}")
 
-    dataframes = []
-    for file_name in file_names:
-        df = download_file(bucket, file_name)
+    df = download_file(bucket, most_recent_file)
+    return df.columns.tolist()
 
-        if df.empty:
-            log(f"File {file_name} is empty. Skipping...")
-            continue
 
-        if len(dataframes) == 0:
-            df.reset_index(inplace=True)
-            dataframes.append(df)
-            continue
+@task(max_retries=3, retry_delay=timedelta(seconds=30))
+def upload_consistent_files(
+    file_name: str,
+    expected_schema: list,
+    environment: str,
+    dataset_id: str,
+    table_id: str,
+    inadequency_threshold: float = 0.2,
+) -> pd.DataFrame:
+    client = storage.Client()
+    bucket_name = constants.GCS_BUCKET.value[environment]
+    bucket = client.bucket(bucket_name)
+    
+    # Download file
+    df = download_file(bucket, file_name)
 
-        if verify_columns_consistency(df.columns.tolist(), dataframes[-1].columns.tolist()):
-            df.reset_index(inplace=True)
-            dataframes.append(df)
-        else:
-            log(
-                f"One of the files {file_name} has columns that are not consistent with the previous files",  # noqa: E501
-                level="error",
-            )
-            raise ValueError(
-                f"One of the files {file_name} has columns that are not consistent with the previous files"  # noqa: E501
-            )
-    df_final = pd.concat(dataframes, ignore_index=True)
+    # Check if the file is empty
+    if df is None or df.empty:
+        log(f"File {file_name} is empty. Skipping...", level="error")
+        return None
 
-    return df_final
+    # Calculate missing columns
+    missing_columns = set(expected_schema) - set(df.columns.tolist())
+    log(f"Missing columns: {missing_columns}. Filling with None.")
+
+    # Fill missing columns with None
+    for column in missing_columns:
+        df[column] = None
+
+    # Calculate extra columns
+    extra_columns = set(df.columns.tolist()) - set(expected_schema)
+    log(f"Extra columns: {extra_columns}. Dropping them.")
+
+    # Drop extra columns
+    df = df.drop(columns=extra_columns)
+
+    inadequency_index = (len(missing_columns) + len(extra_columns)) / len(expected_schema)
+
+    if inadequency_index < inadequency_threshold:
+        upload_df_to_datalake.run(
+            df=df,
+            partition_column="_loaded_at",
+            dataset_id=dataset_id,
+            table_id=table_id,
+            source_format="parquet",
+            dump_mode="append",
+            if_exists="append",
+        )
+
+    return {
+        "file_name": file_name,
+        "inadequency_index": inadequency_index,
+        "missing_columns": missing_columns,
+        "extra_columns": extra_columns,
+        "loaded": inadequency_index < inadequency_threshold,
+    }
+
+@task(max_retries=3, retry_delay=timedelta(seconds=30))
+def report_inadequency(file_pattern: str, reports: list[dict]):
+    loaded_reports = [report for report in reports if report["loaded"]]
+    not_loaded_reports = [report for report in reports if not report["loaded"]]
+
+    message = []
+
+    message.append(f"## Padrão de Arquivos: `{file_pattern}`")
+    message.append(f"- Total de arquivos: {len(reports)}")
+    message.append(f"- Total de arquivos carregados: {len(loaded_reports)}")
+    message.append(f"- Total de arquivos não carregados: {len(not_loaded_reports)}")
+
+    for report in not_loaded_reports:
+        message.append(f"- Arquivo: {report['file_name']}")
+        message.append(f"  - Inadequência: {report['inadequency_index']}")
+        message.append(f"  - Colunas ausentes: {report['missing_columns']}")
+        message.append(f"  - Colunas extras: {report['extra_columns']}")
+
+    if len(not_loaded_reports) > 0:
+        log("\n".join(message), level="error")
+    else:
+        log("\n".join(message))
