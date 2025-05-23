bf48374afc6ecfc16d41676cf232a98fcfed2713
Pedro <pedrovitor.marques@gmail.com>
2025-05-23 16:35:21 -0300

    tmp: add block to test cloud-proxy

--- pipelines/datalake/extract_load/vitacare_backup_mensal_sqlserver/utils.py
+++ pipelines/datalake/extract_load/vitacare_backup_mensal_sqlserver/utils.py
@@ -3,45 +3,37 @@ import subprocess
 import threading
 import time
 
-import prefect
+from pipelines.utils.logger import log
 
 
-def _pipe_output(stream, prefix, logger):
+def _pipe_output(stream, prefix):
     for line in iter(stream.readline, b""):
         message = f"{prefix}: {line.decode(errors='replace').rstrip()}"
-        logger.info(message)
+        print(message)
 
 
 def start_cloud_sql_proxy(connection_name: str) -> subprocess.Popen:
-    logger = prefect.context.get("logger")
-    logger.info(f"Starting Cloud SQL Proxy for connection: {connection_name}")
+    log(f"Starting Cloud SQL Proxy for connection: {connection_name}")
 
-    # Testing if cloud-sql-proxy is installed
-    subprocess.run(["cloud-sql-proxy", "--version"])
+    cwd = [
+        "cloud-sql-proxy",
+        connection_name,
+        "--credentials-file=/tmp/credentials.json",
+        "--port=1433",
+    ]
 
-    process = subprocess.Popen(
-        [
-            "cloud-sql-proxy",
-            connection_name,
-            "--credentials-file=/tmp/credentials.json",
-            "--port=1433",
-        ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        bufsize=1,
-    )
+    process = subprocess.Popen(cwd, stdout=subprocess.PIPE)
+    for line in process.stdout:
+        log(line.decode())
 
-    # Threads para logar stdout e stderr usando o logger do Prefect
-    threading.Thread(
-        target=_pipe_output, args=(process.stdout, "STDOUT", logger), daemon=True
-    ).start()
-    threading.Thread(
-        target=_pipe_output, args=(process.stderr, "STDERR", logger), daemon=True
-    ).start()
+    process.stdout.close()
+    return_code = process.wait()
 
-    time.sleep(5)
+    if return_code:
+        raise subprocess.CalledProcessError(return_code, cwd)
 
     return process
+
 
 
 def stop_cloud_sql_proxy(proxy_process: subprocess.Popen):
