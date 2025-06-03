6612a1468807bfb13a14b6d71849676b1f2c88bd
Daniel Lira <daniellira@Daniels-MacBook-Air.local>
2025-06-03 09:10:18 -0300

    fix backup removing accents

--- pipelines/datalake/extract_load/vitacare_backup_mensal_sqlserver/tasks.py
+++ pipelines/datalake/extract_load/vitacare_backup_mensal_sqlserver/tasks.py
@@ -82,7 +82,7 @@ def extract_and_transform_table(
         for col in df.select_dtypes(include=["object", "string"]).columns:
             current_col_data = df[col].astype(str)
             current_col_data = current_col_data.str.replace(r"[\n\r\t\x00]+", " ", regex=True)
-            current_col_data = current_col_data.str.replace('"', '""', regex=False) # Escape double quotes for CSV
+            current_col_data = current_col_data.str.replace('"', '""', regex=False) 
             df[col] = '"' + current_col_data + '"' # Force quoting
         log(f"All string columns for {full_table_name} (CNES {cnes_code}) prepared for CSV.")
 
