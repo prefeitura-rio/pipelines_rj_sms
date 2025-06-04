9a8d3f42de7b7b413d1e7a913a21c2f01bde3a05
DayaneRamos <dayaneramos.dev@gmail.com>
2025-06-04 15:55:38 -0300

    feat: inserção de dados do cnes aps e acesso mais seguro

--- pipelines/datalake/extract_load/subpav_mysql/schedules.py
+++ pipelines/datalake/extract_load/subpav_mysql/schedules.py
@@ -15,7 +15,10 @@ from pipelines.datalake.extract_load.subpav_mysql.constants import (
 )
 from pipelines.utils.schedules import generate_dump_api_schedules, untuple_clocks
 
-flow_parameters = [
+# ----------------------
+# Parâmetros MENSAIS
+# ----------------------
+monthly_flow_parameters = [
     {
         "table_id": "bairros",
         "dataset_id": subpav_constants.DATASET_ID.value,
@@ -25,24 +28,105 @@ flow_parameters = [
         "schema": "subpav_principal",
     },
     {
-        "table_id": "competencias",
+        "table_id": "indicadores",
+        "dataset_id": subpav_constants.DATASET_ID.value,
+        "environment": "prod",
+        "rename_flow": True,
+        "mode": "replace",
+        "schema": "subpav_indicadores",
+    },
+]
+
+monthly_clocks = generate_dump_api_schedules(
+    interval=timedelta(weeks=4),  # Equivalente a mensal
+    start_date=datetime(2025, 6, 1, 6, 0, tzinfo=pytz.timezone("America/Sao_Paulo")),
+    labels=[constants.RJ_SMS_AGENT_LABEL.value],
+    flow_run_parameters=monthly_flow_parameters,
+    runs_interval_minutes=0,
+)
+
+subpav_monthly_update_schedule = Schedule(clocks=untuple_clocks(monthly_clocks))
+
+
+# ----------------------
+# Parâmetros SEMANAIS
+# ----------------------
+
+# CNES APS
+tables = [
+    "competencias",
+    "unidades",
+    "horarios_atendimentos",
+    "unidades_auxiliares",
+    "unidades_motivos_desativacoes",
+    "equipes",
+    "equipes_tipos",
+    "equipes_profissionais",
+    "profissionais",
+    "profissionais_unidades",
+    "categorias_profissionais",
+    "cbos",
+    "cbos_categorias_profissionais",
+]
+weekly_flow_parameters = [
+    {
+        "table_id": tabela,
         "dataset_id": subpav_constants.DATASET_ID.value,
         "environment": "prod",
         "rename_flow": True,
         "mode": "replace",
         "schema": "subpav_cnes",
-    },
+    }
+    for tabela in tables
 ]
 
-
-clocks = generate_dump_api_schedules(
+weekly_clocks = generate_dump_api_schedules(
     interval=timedelta(weeks=1),
-    start_date=datetime(2025, 1, 1, 5, 0, tzinfo=pytz.timezone("America/Sao_Paulo")),
-    labels=[
-        constants.RJ_SMS_AGENT_LABEL.value,
-    ],
-    flow_run_parameters=flow_parameters,
+    start_date=datetime(2025, 6, 1, 5, 0, tzinfo=pytz.timezone("America/Sao_Paulo")),
+    labels=[constants.RJ_SMS_AGENT_LABEL.value],
+    flow_run_parameters=weekly_flow_parameters,
     runs_interval_minutes=0,
 )
 
-subpav_weekly_update_schedule = Schedule(clocks=untuple_clocks(clocks))
+
+# ----------------------
+# Parâmetros POR MINUTO
+# ----------------------
+
+# AMS - Acesso Mais Seguro
+tables = [
+    "consequencias",
+    "eventos",
+    "locais",
+    "notificacoes",
+    "notificacoes_consequencias",
+    "notificacoes_eventos",
+    "notificacoes_protagonistas",
+    "protagonistas",
+    "riscos",
+    "status",
+    "notificacoes_equipes",
+]
+per_minute_flow_parameters = [
+    {
+        "table_id": tabela,
+        "dataset_id": subpav_constants.DATASET_ID.value,
+        "environment": "prod",
+        "rename_flow": True,
+        "mode": "replace",
+        "schema": "subpav_acesso_mais_seguro",
+    }
+    for tabela in tables
+]
+
+per_minute_clocks = generate_dump_api_schedules(
+    interval=timedelta(minutes=1),
+    start_date=datetime(2025, 6, 1, 5, 0, tzinfo=pytz.timezone("America/Sao_Paulo")),
+    labels=[constants.RJ_SMS_AGENT_LABEL.value],
+    flow_run_parameters=per_minute_flow_parameters,
+    runs_interval_minutes=0,
+)
+
+subpav_combined_schedule = Schedule(
+    clocks=untuple_clocks(monthly_clocks + weekly_clocks + per_minute_clocks)
+)
