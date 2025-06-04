177f100142da78cfb2d84b44eeab53321affb6d2
Matheus Avellar <matheus.avellar@dados.rio>
2025-06-04 11:15:05 -0300

    chore: filtra por artigos relevantes

--- pipelines/datalake/extract_load/diario_oficial_rj/tasks.py
+++ pipelines/datalake/extract_load/diario_oficial_rj/tasks.py
@@ -9,6 +9,8 @@ import pandas as pd
 import pytz
 
 from pipelines.datalake.extract_load.diario_oficial_rj.utils import (
+    get_links_for_path,
+    get_links_if_match,
     get_today,
     node_cleanup,
     send_get_request,
@@ -29,13 +31,13 @@ def get_current_DO_identifiers(date: Optional[str], env: Optional[str]) -> List[
 
     # Precisamos pegar o identificador do DO do dia de hoje
     # Para isso, fazemos uma busca na API no formato:
-    # https://doweb.rio.rj.gov.br/busca/busca/buscar/query/0/di:2025-06-02/df:2025-06-02/?q=%22Secretaria%20Municipal%20de%20Sa%C3%BAde%22
+    # https://doweb.rio.rj.gov.br/busca/busca/buscar/query/0/di:2025-06-02/df:2025-06-02/?q=%22rio%22
 
     BASE = "https://doweb.rio.rj.gov.br/busca/busca/buscar/query/0/"
     DATE_INTERVAL = f"di:{date}/df:{date}/"
     # Precisamos de algum texto de busca, então buscamos
     # por "Secretaria Municipal de Saúde", entre aspas
-    QUERY = "?q=" + urllib.parse.quote('"Secretaria Municipal de Saúde"')
+    QUERY = "?q=" + urllib.parse.quote('"rio"')
     URL = f"{BASE}{DATE_INTERVAL}{QUERY}"
     log(f"Fetching DO for date '{date}'")
 
@@ -45,8 +47,8 @@ def get_current_DO_identifiers(date: Optional[str], env: Optional[str]) -> List[
     # Porém temos campos de metadadaos que indicam quantas instâncias foram encontradas
     # em cada edição, e isso nos dá os IDs que queremos:
     distinct_ids = set()
-    for id in json["aggregations"]["Edicoes"]["buckets"]:
-        distinct_ids.add(id["key"])
+    for edition in json["aggregations"]["Edicoes"]["buckets"]:
+        distinct_ids.add(edition["key"])
     distinct_ids = list(distinct_ids)
 
     log(f"Found {len(distinct_ids)} distinct DO(s) on specified date: {distinct_ids}")
@@ -60,14 +62,15 @@ def get_article_names_ids(diario_id: str) -> List[tuple]:
     # Faz requisição GET, recebe HTML
     html = send_get_request(URL, "html")
 
-    # Precisamos encontrar todas as instâncias de
+    # Precisamos encontrar todas as instâncias relevantes de
     # <a class="linkMateria" identificador="..." pagina="" data-id="..." data-protocolo="..." data-materia-id="...">
-    # De onde vamos extrair `identificador` ou `data-materia-id`
-    REGEX_DIGITS = re.compile(r"[0-9]+")
-    results = set(
-        html.find_all("a", attrs={"identificador": REGEX_DIGITS})
-        + html.find_all("a", attrs={"data-materia-id": REGEX_DIGITS})
-    )
+    # De onde queremos extrair `identificador` ou `data-materia-id`
+    all_folders = html.find_all("span", attrs={"class": "folder"})
+    results = []
+    results.extend(get_links_for_path(all_folders, ["atos do prefeito", "decretos n"]))
+    results.extend(get_links_for_path(all_folders, ["secretaria municipal de saúde", "resoluções", "resolução n"]))
+    results.extend(get_links_if_match(all_folders, r"^controladoria geral"))
+    results.extend(get_links_if_match(all_folders, r"^tribunal de contas"))
 
     # Não temos como garantir que ambos os atributos vão existir sempre;
     # então pegamos o valor do primeiro preenhcido que encontrarmos
@@ -87,11 +90,10 @@ def get_article_names_ids(diario_id: str) -> List[tuple]:
         set(
             (tag.text.strip(), get_any_attribute(tag, ["identificador", "data-materia-id"]))
             for tag in results
-            # if re.search(r"decreto rio", tag.text, re.IGNORECASE)
         )
     )
-    log(f"Found {len(results)} articles; filtered to {len(filtered_results)} articles")
-    return filtered_results[:100]  # FIXME
+    log(f"Found {len(filtered_results)} relevant articles")
+    return filtered_results
 
 
 @task(max_retries=3, retry_delay=timedelta(seconds=30))
@@ -105,6 +107,8 @@ def get_article_contents(article: tuple) -> dict:
     URL = f"https://doweb.rio.rj.gov.br/apifront/portal/edicoes/publicacoes_ver_conteudo/{id}"
     # Faz requisição GET, recebe HTML
     html = send_get_request(URL, "html")
+    # Registra data/hora da extração
+    current_datetime = datetime.datetime.now(tz=pytz.timezone("America/Sao_Paulo"))
     # Salva o HTML cru do corpo
     body_html = string_cleanup(html.body)
     # Remove elementos inline comuns (<b>, <i>, <span>) pra não
@@ -114,16 +118,23 @@ def get_article_contents(article: tuple) -> dict:
     full_text = string_cleanup(html.body.get_text(separator="\n", strip=True))
 
     log(f"Article '{title}' (id '{id}') has size {len(full_text)} ({len(body_html)} with HTML)")
-    return {"titulo": title, "texto": full_text, "html": body_html}
+    return {
+        "titulo": title,
+        "texto": full_text,
+        "html": body_html,
+        "_extracted_at": current_datetime
+    }
 
 
 @task(max_retries=3, retry_delay=timedelta(seconds=30))
 def upload_results(results_list: List[dict], dataset: str):
+    if len(results_list) == 0:
+        log(f"Nothing to upload; leaving")
+        return
+
     main_df = pd.DataFrame()
-    current_datetime = datetime.datetime.now(tz=pytz.timezone("America/Sao_Paulo"))
 
     for result in results_list:
-        result["_extracted_at"] = current_datetime
         single_df = pd.DataFrame.from_records([result])
         main_df = pd.concat([main_df, single_df], ignore_index=True)
 
--- pipelines/datalake/extract_load/diario_oficial_rj/utils.py
+++ pipelines/datalake/extract_load/diario_oficial_rj/utils.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-from typing import Optional
+from typing import List, Optional
 
 import pytz
+import re
 import requests
 from bs4 import BeautifulSoup, NavigableString
 from dateutil import parser
@@ -41,6 +42,69 @@ def send_get_request(url: str, type: Optional[str]):
         return BeautifulSoup(res.text, "html.parser")
 
     return res.text
+
+
+def get_all_links_in_folder(folder: BeautifulSoup) -> List[BeautifulSoup]:
+    REGEX_DIGITS = re.compile(r"[0-9]+")
+    # A estrutura é:
+    # <li ...>
+    #   <span class="folder"> ... </span>
+    #   <ul ...>
+    #     <!-- Cada li pode ser uma sub-pasta; eventualmente há um 'file' -->
+    #     <li>
+    #       <span class="file">
+    #         <a class="linkMateria" ...> ... </a>
+    #       </span>
+    #     </li>
+    #   </ul>
+    # </li>
+    root = folder.parent
+    return list(
+        set(
+            root.find_all("a", attrs={"identificador": REGEX_DIGITS})
+            + root.find_all("a", attrs={"data-materia-id": REGEX_DIGITS})
+        )
+    )
+
+
+def get_links_for_path(folders: List[BeautifulSoup], path: list) -> List[BeautifulSoup]:
+    # Se chegamos no final da recursão
+    if len(path) == 0:
+        # Para cada pasta que recebemos
+        links = []
+        for folder in folders:
+            # Pega todos os links nela, e guarda junto com os outros
+            links.extend(get_all_links_in_folder(folder))
+        return links
+
+    # Se ainda temos mais recursão pela frente, pega a primeira pasta do caminho
+    first_folder_name = path[0]
+    # Para cada pasta que recebemos
+    for folder in folders:
+        # Pega o 'nome' da pasta
+        text = folder.get_text().lower().strip()
+        # Confere se bate com o que queremos
+        if text == first_folder_name:
+            # Se sim, pega somente as pastas dentro dessa
+            subfolders = folder.parent.find_all("span", attrs={"class": "folder"})
+            # ...e remove a própria do caminho
+            return get_links_for_path(subfolders, path[1:])
+    # Se chegamos aqui, não encontramos a pasta especificada pelo caminho
+    return []
+
+
+def get_links_if_match(folders: List[BeautifulSoup], regex_search_str: str) -> List[BeautifulSoup]:
+    all_links = []
+    for folder in folders:
+        # Pega o 'nome' da pasta
+        text = folder.get_text().strip()
+        # Confere se bate com o que queremos
+        if re.search(regex_search_str, text, re.IGNORECASE | re.MULTILINE):
+            # Pega todos os <a> com atributos que estamos procurando
+            # que estão 'dentro' da pasta
+            all_links.extend(get_all_links_in_folder(folder))
+
+    return all_links
 
 
 def string_cleanup(string: str) -> str:
