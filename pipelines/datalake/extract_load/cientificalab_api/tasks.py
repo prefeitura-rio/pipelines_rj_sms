b2a09ce25b38fbe8e0818d567e05a1c6ed72a6ea
Daniel Lira <danielllira@icloud.com>
2025-07-08 15:46:13 -0300

    feat: add dt_inicio, dt_fim to schedules.py paramns

--- pipelines/datalake/extract_load/cientificalab_api/tasks.py
+++ pipelines/datalake/extract_load/cientificalab_api/tasks.py
@@ -23,7 +23,7 @@ def authenticate_and_fetch(
 ):
     token_response = cloud_function_request.run(
         url="https://cielab.lisnet.com.br/lisnetws/tokenlisnet/apccodigo",
-        request_type="POST",
+        request_type="GET",
         query_params={
             "Content-Type": "application/json",
             "apccodigo": apccodigo,
@@ -66,7 +66,7 @@ def authenticate_and_fetch(
 
     resultado_response = cloud_function_request.run(
         url="https://cielab.lisnet.com.br/lisnetws/APOIO/DTL/resultado",
-        request_type="POST",
+        request_type="GET",
         query_params={
             "codigo": apccodigo,
             "token": token,
