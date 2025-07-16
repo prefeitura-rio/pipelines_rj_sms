26d7ccde93435f38c290364c265b874e2ad68022
Daniel Lira <danielllira@icloud.com>
2025-07-16 14:45:30 -0300

    raise memory limit

--- pipelines/datalake/extract_load/cientificalab_api/flows.py
+++ pipelines/datalake/extract_load/cientificalab_api/flows.py
@@ -156,7 +156,7 @@ flow_cientificalab_operator.run_config = KubernetesRun(
     labels=[
         constants.RJ_SMS_AGENT_LABEL.value,
     ],
-    memory_limit="4Gi",
+    memory_limit="6Gi",
 )
 
 flow_cientificalab_manager.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
