9699343877ffc1e8479fa8d1ff3a28730f46e653
HamsterGulloso <danielgullovasconcelos@gmail.com>
2025-08-08 15:53:14 -0300

    feat: creation and alterations of a series of tasks, used for: finding the limits of a table; generating the value used for slicing; querying using the limits found; joining the results in a single object.

--- pipelines/datalake/extract_load/ser_metabase/constants.py
+++ pipelines/datalake/extract_load/ser_metabase/constants.py
@@ -7,25 +7,82 @@ DATABASE_IDS = {
         "id": 178,  # Banco novo: Novas tabelas com diversos schemas que apresentam aparentes
         # melhoras nos dados disponibilizados.
         "tables": {
-            "DIM_RECURSO": 3476,  # Lista dos recursos cadastrados
-            "DIM_UNIDADE": 3477,  # Lista das unidades cadastradas
-            "FATO_AMBULATORIO": 3255,  # Solicitações ambulatoriais
-            "FATO_HISTORICO_SOLICITACAO": 3260,  # Lista os eventos de todas as solicitações
-            "FATO_INTERNACAO": 3259,  # Solicitações de Internação
-            "FATO_MAPA_LEITOS": 3258,  # (Descrição ausente)
-            "FATO_FILA_MENSAL": 5872,  # (Descrição ausente)
-            "FATO_FILA_MENSAL_DATALAKE": 5873,  # (Descrição ausente)
-            "TB_RELATORIO_MEDICAMENTOS": 3478,  # (Descrição ausente)
-            "TB_RELATORIO_DISPENSACAO_NOVO": 3479,  # (Descrição ausente)
+            "DIM_RECURSO": {
+                "id": 3476,  # Lista dos recursos cadastrados
+                "slice_column": ""
+            },
+            "DIM_UNIDADE": {
+                "id": 3477,  # Lista das unidades cadastradas
+                "slice_column": ""
+            },
+            "FATO_AMBULATORIO": {
+                "id": 3255,  # Solicitações ambulatoriais
+                "slice_column": ""
+            },
+            "FATO_HISTORICO_SOLICITACAO": {
+                "id": 3260,  # Lista os eventos de todas as solicitações
+                "slice_column": ""
+            },
+            "FATO_INTERNACAO": {
+                "id": 3259,  # Solicitações de Internação
+                "slice_column": ""
+            },
+            "FATO_MAPA_LEITOS": {
+                "id": 3258,  # (Descrição ausente)
+                "slice_column": ""
+            },
+            "FATO_FILA_MENSAL": {
+                "id": 5872,  # (Descrição ausente)
+                "slice_column": ""
+            },
+            "FATO_FILA_MENSAL_DATALAKE": {
+                "id": 5873,  # (Descrição ausente)
+                "slice_column": ""
+            },
+            "TB_RELATORIO_MEDICAMENTOS": {
+                "id": 3478,  # (Descrição ausente)
+                "slice_column": ""
+            },
+            "TB_RELATORIO_DISPENSACAO_NOVO": {
+                "id": 3479,  # (Descrição ausente)
+                "slice_column": ""
+            },
         },
     },
     "REGULACAO_METRO1_CAPITAL": {
         "id": 173,  # Banco com tabelas que estavam sendo utilizadas, mas apresentam
         # diversos problemas como inconsistência e falta de informações.
         "tables": {
-            "TB_HISTORICO_SOLICITACAO_METRO1_CAPITAL": 3262,
-            "TB_QUANTIDADE_SOLICITACOES": 3261,
-            "TB_SOLICITACOES": 5783,
+            "TB_HISTORICO_SOLICITACAO_METRO1_CAPITAL": {
+                "id": 3262,
+                "slice_column": ""
+            },
+            "TB_QUANTIDADE_SOLICITACOES": {
+                "id": 3261,
+                "slice_column": ""
+            },
+            "TB_SOLICITACOES": {
+                "id": 5783,
+                "slice_column": ""
+            },
         },
     },
 }
+
+
+SLICE_COLUMNS: int = {
+    173: {
+        5783: 51407
+    },
+}
+"""
+dict[int, dict[int, int]]
+
+indexado por `database_id` e `table_id`
+
+uso:
+    database_id = 173
+    table_id = 5783
+
+    column_id = SLICE_COLUMNS[database_id][table_id]
+"""
--- pipelines/datalake/extract_load/ser_metabase/flows.py
+++ pipelines/datalake/extract_load/ser_metabase/flows.py
@@ -7,6 +7,7 @@ from prefect import Parameter
 from prefect.executors import LocalDaskExecutor
 from prefect.run_configs import KubernetesRun
 from prefect.storage import GCS
+from prefect.utilities.edges import unmapped
 
 # Internos
 from prefeitura_rio.pipelines_utils.custom import Flow
@@ -16,9 +17,12 @@ from pipelines.datalake.extract_load.ser_metabase.schedules import schedule
 from pipelines.datalake.extract_load.ser_metabase.tasks import (
     authenticate_in_metabase,
     interrupt_if_empty,
-    query_database,
+    calculate_slices,
+    query_slice_limit,
+    query_database_slice,
+    join_slices,
 )
-from pipelines.datalake.utils.tasks import handle_columns_to_bq
+# from pipelines.datalake.utils.tasks import handle_columns_to_bq
 from pipelines.utils.tasks import get_secret_key, upload_df_to_datalake
 
 with Flow("SUBGERAL - Extract & Load - SER METABASE") as ser_metabase_flow:
@@ -29,13 +33,27 @@ with Flow("SUBGERAL - Extract & Load - SER METABASE") as ser_metabase_flow:
     TABLE_ID = Parameter("table_id", default=3255, required=True)
 
     # BIGQUERY ------------------------------
-    BQ_DATASET_ID = Parameter("bq_dataset_id", default="brutos_ser_metabase", required=True)
-    BQ_TABLE_ID = Parameter("bq_table_id", default="FATO_AMBULATORIO", required=True)
+    BQ_DATASET_ID = Parameter(
+        "bq_dataset_id",
+        default="brutos_ser_metabase",
+        required=True
+    )
+    BQ_TABLE_ID = Parameter(
+        "bq_table_id",
+        default="FATO_AMBULATORIO",
+        required=True
+    )
 
     # CREDENTIALS ------------------------------
-    user = get_secret_key(environment=ENVIRONMENT, secret_name="USER", secret_path="/metabase")
+    user = get_secret_key(
+        environment=ENVIRONMENT,
+        secret_name="USER",
+        secret_path="/metabase"
+    )
     password = get_secret_key(
-        environment=ENVIRONMENT, secret_name="PASSWORD", secret_path="/metabase"
+        environment=ENVIRONMENT,
+        secret_name="PASSWORD",
+        secret_path="/metabase"
     )
 
     # Task 1 - Authenticate in Metabase
@@ -44,20 +62,54 @@ with Flow("SUBGERAL - Extract & Load - SER METABASE") as ser_metabase_flow:
         password=password,
     )
 
-    # Task 2 - Query Database
-    df = query_database(
+    # Task 2 - Calculate minimal value for data slicing
+    min_value = query_slice_limit(
         token=token,
         database_id=DATABASE_ID,
         table_id=TABLE_ID,
+        which='min'
+    )
+    # Task 3 - Calculate maximal value for data slicing
+    max_value = query_slice_limit(
+        token=token,
+        database_id=DATABASE_ID,
+        table_id=TABLE_ID,
+        which='max'
     )
 
-    # Task 3 - Verify Database length
-    df_verified = interrupt_if_empty(df=df)
+    # Task 4 - Calculate list of inital values for each slice
+    slices_min = calculate_slices(
+        min_value=min_value,
+        max_value=max_value,
+        which='min'
+    )
+    # Task 5 - Calculate list of final values for each slice
+    slices_max = calculate_slices(
+        min_value=min_value,
+        max_value=max_value,
+        which='max'
+    )
 
-    # Task 4 - Transform Data Frame columns
-    df_columns_ok = handle_columns_to_bq(df=df_verified)
+    # Task 6 - Queries the data for each slice determined
+    dfs = query_database_slice.map(
+        token=unmapped(token),
+        database_id=unmapped(DATABASE_ID),
+        table_id=unmapped(TABLE_ID),
+        slice_min=slices_min,
+        slice_max=slices_max,
+    )
 
-    # Task 5 - Upload to Big Query
+    # Task 7 - Concats the dataframes resulted from the previous tasks
+    df = join_slices(
+        dfs=dfs
+    )
+
+    # Task 8 - Verify Database length
+    df_columns_ok = interrupt_if_empty(
+        df=df
+    )
+
+    # Task 9 - Upload to Big Query
     upload_df_to_datalake(
         df=df_columns_ok,
         table_id=BQ_TABLE_ID,
