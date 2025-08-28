f706ff2d68d89de70b8b18b8a2cd84893c570c27
Daniel Lira <danielllira@icloud.com>
2025-08-28 07:33:19 -0300

    fix: change dataset_id

--- pipelines/datalake/extract_load/vitacare_api_v2/schedules.py
+++ pipelines/datalake/extract_load/vitacare_api_v2/schedules.py
@@ -17,7 +17,7 @@ routine_flow_parameters = [
     # Execuções do Dia
     # ------------------------------------------------------------
     {
-        "dataset_id": "brutos_prontuario_vitacare_api_centralizadora",
+        "dataset_id": "brutos_prontuario_vitacare_api",
         "endpoint": "posicao",
         "environment": "prod",
         "rename_flow": True,
@@ -25,7 +25,7 @@ routine_flow_parameters = [
         "target_date": "D-0",
     },
     {
-        "dataset_id": "brutos_prontuario_vitacare_api_centralizadora",
+        "dataset_id": "brutos_prontuario_vitacare_api",
         "endpoint": "movimento",
         "environment": "prod",
         "rename_flow": True,
@@ -33,7 +33,7 @@ routine_flow_parameters = [
         "target_date": "D-1",
     },
     {
-        "dataset_id": "brutos_prontuario_vitacare_api_centralizadora",
+        "dataset_id": "brutos_prontuario_vitacare_api",
         "endpoint": "vacina",
         "environment": "prod",
         "rename_flow": True,
@@ -44,7 +44,7 @@ routine_flow_parameters = [
     # Execuções de Redundância
     # ------------------------------------------------------------
     {
-        "dataset_id": "brutos_prontuario_vitacare_api_centralizadora",
+        "dataset_id": "brutos_prontuario_vitacare_api",
         "endpoint": "movimento",
         "environment": "prod",
         "rename_flow": True,
