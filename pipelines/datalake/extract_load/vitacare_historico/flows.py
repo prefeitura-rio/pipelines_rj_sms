1a888c76e792524b405d3951e9d94038a7e5b19c
Daniel Lira <danielllira@icloud.com>
2025-09-04 16:36:18 -0300

    fix

--- pipelines/datalake/extract_load/vitacare_historico/flows.py
+++ pipelines/datalake/extract_load/vitacare_historico/flows.py
@@ -82,7 +82,7 @@ with Flow(
             env=ENVIRONMENT,
         )
 
-    cnes_to_process = cnes_to_process = ["5154197_20250902_175554"]
+    cnes_to_process = ["5154197_20250902_175554"]
 
     process_cnes_table.map(
         db_host=unmapped(db_host),
