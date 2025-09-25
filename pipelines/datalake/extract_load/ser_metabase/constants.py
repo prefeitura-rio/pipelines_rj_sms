eb12708987f9e6dad1ed2f9ef61368876e6485b7
HamsterGulloso <danielgullovasconcelos@gmail.com>
2025-09-25 17:06:34 -0300

    feat: Added more constants to enable more extractions

--- pipelines/datalake/extract_load/ser_metabase/constants.py
+++ pipelines/datalake/extract_load/ser_metabase/constants.py
@@ -11,30 +11,68 @@ DATABASE_IDS = {
         # aparentes melhoras nos dados disponibilizados.
         "tables": {
             # Lista dos recursos cadastrados
-            "DIM_RECURSO": {"id": 3476, "slice_column": ""},
+            "DIM_RECURSO": {
+                "id": 3476,
+                "slice_column": "recurso_id"
+            },
             # Lista das unidades cadastradas
-            "DIM_UNIDADE": {"id": 3477, "slice_column": ""},
+            "DIM_UNIDADE": {
+                "id": 3477,
+                "slice_column": "UNIDADE_ID"
+            },
+            # Solicitações ambulatoriais
             "FATO_AMBULATORIO": {
-                "id": 3255,  # Solicitações ambulatoriais
+                "id": 3255,
                 "slice_column": "solicitacao_id",
             },
+            # Solicitações ambulatoriais
+            "FATO_AMBULATORIO_2019": {
+                "id": 5938,
+                "slice_column": "solicitacao_id",
+            },
+            # Lista os eventos de todas as solicitações
+            "FATO_HISTORICO_2019_ON": {
+                "id": 5943,
+                "slice_column": "solicitacao_id",
+            },
+            # Lista os eventos de todas as solicitações
             "FATO_HISTORICO_SOLICITACAO": {
-                "id": 3260,  # Lista os eventos de todas as solicitações
+                "id": 3260,
                 "slice_column": "solicitacao_id",
             },
+            # Solicitações de Internação
             "FATO_INTERNACAO": {
-                "id": 3259,  # Solicitações de Internação
+                "id": 3259,
                 "slice_column": "solicitacao_id",
             },
-            "FATO_MAPA_LEITOS": {"id": 3258, "slice_column": ""},  # (Descrição ausente)
-            "FATO_FILA_MENSAL": {"id": 5872, "slice_column": ""},  # (Descrição ausente)
-            "FATO_FILA_MENSAL_DATALAKE": {"id": 5873, "slice_column": ""},  # (Descrição ausente)
-            "TB_RELATORIO_MEDICAMENTOS": {
-                "id": 3478,  # (Descrição ausente)
+            # (Descrição ausente)
+            "FATO_INTERNACAO_2019": {
+                "id": 5940,
+                "slice_column": "solicitacao_id",
+            },
+            # (Descrição ausente)
+            "Fato_Mapa_leitos": {
+                "id": 3258,
                 "slice_column": "",
             },
+            # (Descrição ausente)
+            "fato_fila_mensal": {
+                "id": 5872,
+                "slice_column": ""
+            },
+            # (Descrição ausente)
+            "fato_fila_mensal_datalake": {
+                "id": 5873,
+                "slice_column": ""
+            },
+            # (Descrição ausente)
+            "TB_RELATORIO_MEDICAMENTOS": {
+                "id": 3478,
+                "slice_column": "",
+            },
+            # (Descrição ausente)
             "TB_RELATORIO_DISPENSACAO_NOVO": {
-                "id": 3479,  # (Descrição ausente)
+                "id": 3479,
                 "slice_column": "",
             },
         },
@@ -44,9 +82,18 @@ DATABASE_IDS = {
         # Banco com tabelas que estavam sendo utilizadas, mas apresentam
         # diversos problemas como inconsistência e falta de informações.
         "tables": {
-            "TB_HISTORICO_SOLICITACAO_METRO1_CAPITAL": {"id": 3262, "slice_column": ""},
-            "TB_QUANTIDADE_SOLICITACOES": {"id": 3261, "slice_column": ""},
-            "TB_SOLICITACOES": {"id": 5783, "slice_column": "solicitacao_id"},
+            "TB_HISTORICO_SOLICITACAO_METRO1_CAPITAL": {
+                "id": 3262,
+                "slice_column": ""
+            },
+            "TB_QUANTIDADE_SOLICITACOES": {
+                "id": 3261,
+                "slice_column": ""
+            },
+            "TB_SOLICITACOES": {
+                "id": 5783,
+                "slice_column": "solicitacao_id"
+            },
         },
     },
 }
@@ -57,36 +104,102 @@ QUERY_COLUMNS: dict[
     dict[
         int,
         dict[
-            Literal["slice_column", "slice_column_unique", "date_column", "slice_column_type"], any
+            Literal[
+                "slice_column",
+                "slice_column_unique",
+                "date_column",
+                "slice_column_type"
+            ],
+            int | str | bool
         ],
     ],
 ] = {
     173: {
+        # TB_SOLICITACOES
         5783: {
             "slice_column": 51407,
             "slice_column_unique": True,
-            "date_column": 51409,
             "slice_column_type": "String",
+            "date_column": 51409,
         }
     },
     178: {
+        # DIM_RECURSO
+        3476: {
+            "slice_column": 31534,
+            "slice_column_unique": True,
+            "slice_column_type": "Integer",
+            "date_column": None,
+        },
+        # DIM_UNIDADE
+        3477: {
+            "slice_column": 31547,
+            "slice_column_unique": True,
+            "slice_column_type": "Integer",
+            "date_column": None,
+        },
+        # FATO_AMBULATORIO
         3255: {
             "slice_column": 30429,
             "slice_column_unique": True,
+            "slice_column_type": "String",
             "date_column": 30452,
-            "slice_column_type": "String",
         },
-        3259: {
-            "slice_column": 30513,
+        # FATO_AMBULATORIO_2019
+        5938: {
+            "slice_column": 54371,
             "slice_column_unique": True,
-            "date_column": 30522,
-            "slice_column_type": "String",
+            "slice_column_type": "Integer",
+            "date_column": 54371,
         },
+        # FATO_HISTORICO_2019_ON
+        5943: {
+            "slice_column": 54392,
+            "slice_column_unique": False,
+            "slice_column_type": "Integer",
+            "date_column": 54386,
+        },
+        # FATO_HISTORICO_SOLICITACAO
         3260: {
             "slice_column": 30539,
             "slice_column_unique": False,
-            "date_column": 30545,
             "slice_column_type": "Integer",
+            "date_column": 30545,
+        },
+        # FATO_INTERNACAO
+        3259: {
+            "slice_column": 30513,
+            "slice_column_unique": True,
+            "slice_column_type": "String",
+            "date_column": 30522,
+        },
+        # FATO_INTERNACAO_2019
+        5940: {
+            "slice_column": 54424,
+            "slice_column_unique": True,
+            "slice_column_type": "Text",
+            "date_column": 54394,
+        },
+        # Fato_Mapa_leitos
+        3258: {
+            "slice_column": 30479,
+            "slice_column_unique": False,
+            "slice_column_type": "Text",
+            "date_column": 30474,
+        },
+        # fato_fila_mensal
+        5872: {
+            "slice_column": 52380,
+            "slice_column_unique": False,
+            "slice_column_type": "Integer",
+            "date_column": 52416,
+        },
+        # fato_fila_mensal_datalake
+        5873: {
+            "slice_column": 52409,
+            "slice_column_unique": False,
+            "slice_column_type": "Text",
+            "date_column": 52413,
         },
     },
 }
