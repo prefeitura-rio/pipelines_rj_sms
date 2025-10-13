d870197304beff092a2067d971c34e7a01fa0122
Pedro Marques <pedrovitor.marques@gmail.com>
2025-10-13 08:49:23 -0300

    Update constants.py

--- pipelines/datalake/extract_load/vitacare_api_v2/constants.py
+++ pipelines/datalake/extract_load/vitacare_api_v2/constants.py
@@ -15,18 +15,6 @@ class constants(Enum):
     INFISICAL_VITACARE_USERNAME = "USERNAME"
     INFISICAL_VITACARE_PASSWORD = "PASSWORD"
 
-    BASE_URL = {
-        "AP10": "http://consolidado-ap10.pepvitacare.com:8088",
-        "AP21": "http://consolidado-ap21.pepvitacare.com:8090",
-        "AP22": "http://consolidado-ap22.pepvitacare.com:8091",
-        "AP31": "http://consolidado-ap31.pepvitacare.com:8089",
-        "AP32": "http://consolidado-ap32.pepvitacare.com:8090",
-        "AP33": "http://consolidado-ap33.pepvitacare.com:8089",
-        "AP40": "http://consolidado-ap40.pepvitacare.com:8089",
-        "AP51": "http://consolidado-ap51.pepvitacare.com:8091",
-        "AP52": "http://consolidado-ap52.pepvitacare.com:8088",
-        "AP53": "http://consolidado-ap53.pepvitacare.com:8092",
-    }
     ENDPOINT = {
         "posicao": "/reports/pharmacy/stocks",
         "movimento": "/reports/pharmacy/movements",
