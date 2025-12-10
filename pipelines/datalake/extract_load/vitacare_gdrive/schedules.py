d651734c53d6677ca6839c48980d4c7673f49a95
Matheus Avellar <matheus.avellar@dados.rio>
2025-12-10 11:03:48 -0300

    chore: adiciona tabelas requisitadas SAP
    - Ficha A vitahiscare
    - Solicitação Saúde Bucal
    - Relação HASDM

--- pipelines/datalake/extract_load/vitacare_gdrive/schedules.py
+++ pipelines/datalake/extract_load/vitacare_gdrive/schedules.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=C0103
+# flake8: noqa: E501
 """
 Schedules
 """
@@ -14,86 +15,110 @@ from pipelines.utils.schedules import generate_dump_api_schedules, untuple_clock
 
 flow_parameters = [
     {
-        "file_pattern": "INFORMES-MENSAIS-ETSN/AP*/*/REPORT_PLANILHA_ACOMPANHAMENTO_GESTANTES/*/*_SUBPAV_PLAN_ACOMP_MENSAL_GESTANTES.csv",  # noqa: E501
+        "file_pattern": "INFORMES-MENSAIS-ETSN/AP*/*/REPORT_PLANILHA_ACOMPANHAMENTO_GESTANTES/*/*_SUBPAV_PLAN_ACOMP_MENSAL_GESTANTES.csv",
         "desired_dataset_name": "brutos_informes_vitacare",
         "desired_table_name": "acompanhamento_mensal_gestantes",
         "environment": "prod",
         "rename_flow": True,
     },
     {
-        "file_pattern": "INFORMES-MENSAIS-ETSN/AP*/*/REPORT_DISPENSAS_APARELHO_PRESSAO/*/*_DISPENSAS_APARELHO_PRESSAO_*.csv",  # noqa: E501
+        "file_pattern": "INFORMES-MENSAIS-ETSN/AP*/*/REPORT_DISPENSAS_APARELHO_PRESSAO/*/*_DISPENSAS_APARELHO_PRESSAO_*.csv",
         "desired_dataset_name": "brutos_informes_vitacare",
         "desired_table_name": "dispensas_aparelho_pressao",
         "environment": "prod",
         "rename_flow": True,
     },
     {
-        "file_pattern": "INFORMES-MENSAIS-ETSN/AP*/*/REPORT_ACOMPANHAMENTO_MULHERES_IDADE_FERTIL/*/*_ACOMPANHAMENTO_MULHERES_IDADE_FERTIL_*.csv",  # noqa: E501
+        "file_pattern": "INFORMES-MENSAIS-ETSN/AP*/*/REPORT_ACOMPANHAMENTO_MULHERES_IDADE_FERTIL/*/*_ACOMPANHAMENTO_MULHERES_IDADE_FERTIL_*.csv",
         "desired_dataset_name": "brutos_informes_vitacare",
         "desired_table_name": "acompanhamento_mulheres_idade_fertil",
         "environment": "prod",
         "rename_flow": True,
     },
     {
-        "file_pattern": "INFORMES-MENSAIS-ETSN/AP*/*/REPORT_LISTAGEM_VACINA_V2/*/*_LISTAGEM_VACINA_V2_*.csv",  # noqa: E501
+        "file_pattern": "INFORMES-MENSAIS-ETSN/AP*/*/REPORT_LISTAGEM_VACINA_V2/*/*_LISTAGEM_VACINA_V2_*.csv",
         "desired_dataset_name": "brutos_informes_vitacare",
         "desired_table_name": "listagem_vacina_v2",
         "environment": "prod",
         "rename_flow": True,
     },
+
+    # FICHA A
     {
-        "file_pattern": "INFORMES-MENSAIS-ETSN/AP*/*/REPORT_SUB_PAV_FICHA_A_V2/*/*SUB_PAV_FICHA_A_V2*.csv",  # noqa: E501
+        "file_pattern": "INFORMES-MENSAIS-ETSN/AP*/*/REPORT_SUB_PAV_FICHA_A/*/*_*_RELACAO_FICHA_A_VITAhisCARE.csv",
+        "desired_dataset_name": "brutos_informes_vitacare",
+        "desired_table_name": "ficha_a_vitahiscare",
+        "environment": "prod",
+        "rename_flow": True,
+    },
+    {
+        "file_pattern": "INFORMES-MENSAIS-ETSN/AP*/*/REPORT_SUB_PAV_FICHA_A_V2/*/*SUB_PAV_FICHA_A_V2*.csv",
         "desired_dataset_name": "brutos_informes_vitacare",
         "desired_table_name": "ficha_a_v2",
         "environment": "prod",
         "rename_flow": True,
     },
+
     {
-        "file_pattern": "INFORMES-MENSAIS-ETSN/AP*/*/REPORT_SUB_PAV_FICHA_C_CRIANCA_V2/*/*FICHA_C_CRIANCAS_CAP_V2*.csv",  # noqa: E501
+        "file_pattern": "INFORMES-MENSAIS-ETSN/AP*/*/REPORT_SUB_PAV_FICHA_C_CRIANCA_V2/*/*FICHA_C_CRIANCAS_CAP_V2*.csv",
         "desired_dataset_name": "brutos_informes_vitacare",
         "desired_table_name": "ficha_c_v2",
         "environment": "prod",
         "rename_flow": True,
     },
     {
-        "file_pattern": "INFORMES-MENSAIS-ETSN/AP*/*/REPORT_SUB_PAV_RELACAO_CRIANCAS_MENORES_DE_5_ANOS/*/*_RELACAO_CRIANCAS_VITAhisCARE.csv",  # noqa: E501
+        "file_pattern": "INFORMES-MENSAIS-ETSN/AP*/*/REPORT_SUB_PAV_RELACAO_CRIANCAS_MENORES_DE_5_ANOS/*/*_RELACAO_CRIANCAS_VITAhisCARE.csv",
         "desired_dataset_name": "brutos_informes_vitacare",
         "desired_table_name": "criancas_menores_5_anos",
         "environment": "prod",
         "rename_flow": True,
     },
     {
-        "file_pattern": "INFORMES-MENSAIS-ETSN/AP*/*/REPORT_INDICADORES_VARIAVEL_3_G*/*/*INDICADORES_VARIAVEL_3_G*.csv",  # noqa: E501
+        "file_pattern": "INFORMES-MENSAIS-ETSN/AP*/*/REPORT_INDICADORES_VARIAVEL_3_G*/*/*INDICADORES_VARIAVEL_3_G*.csv",
         "desired_dataset_name": "brutos_informes_vitacare",
         "desired_table_name": "indicadores_cg_variavel_3",
         "environment": "prod",
         "rename_flow": True,
     },
     {
-        "file_pattern": "INFORMES-MENSAIS-ETSN/AP*/*/REPORT_INDICADORES_VARIAVEL_2/*/*INDICADORES_VARIAVEL_2*.csv",  # noqa: E501
+        "file_pattern": "INFORMES-MENSAIS-ETSN/AP*/*/REPORT_INDICADORES_VARIAVEL_2/*/*INDICADORES_VARIAVEL_2*.csv",
         "desired_dataset_name": "brutos_informes_vitacare",
         "desired_table_name": "indicadores_cg_variavel_2",
         "environment": "prod",
         "rename_flow": True,
     },
     {
-        "file_pattern": "INFORMES-MENSAIS-ETSN/AP*/*/REPORT_INDICADORES_VARIAVEL_1/*/*INDICADORES_VARIAVEL_1*.csv",  # noqa: E501
+        "file_pattern": "INFORMES-MENSAIS-ETSN/AP*/*/REPORT_INDICADORES_VARIAVEL_1/*/*INDICADORES_VARIAVEL_1*.csv",
         "desired_dataset_name": "brutos_informes_vitacare",
         "desired_table_name": "indicadores_cg_variavel_1",
         "environment": "prod",
         "rename_flow": True,
     },
     {
-        "file_pattern": "INFORMES-MENSAIS-ETSN/AP*/*/REPORT_PLANILHA_ACOMPANHAMENTO_ESTADO_NUTRICIONAL_SISVAN/*/*ESTADO_NUTRICIONAL_SISVAN.csv",  # noqa: E501
+        "file_pattern": "INFORMES-MENSAIS-ETSN/AP*/*/REPORT_PLANILHA_ACOMPANHAMENTO_ESTADO_NUTRICIONAL_SISVAN/*/*ESTADO_NUTRICIONAL_SISVAN.csv",
         "desired_dataset_name": "brutos_informes_vitacare",
         "desired_table_name": "estado_nutricional_sisvan",
         "environment": "prod",
         "rename_flow": True,
     },
     {
-        "file_pattern": "INFORMES-MENSAIS-ETSN/AP*/2025-*/REPORT_LISTAGEM_ATENDIMENTOS_PACIENTES_TEA/*/*_LISTAGEM_ATENDIMENTOS_PACIENTES_TEA*.csv",  # noqa: E501
+        "file_pattern": "INFORMES-MENSAIS-ETSN/AP*/2025-*/REPORT_LISTAGEM_ATENDIMENTOS_PACIENTES_TEA/*/*_LISTAGEM_ATENDIMENTOS_PACIENTES_TEA*.csv",
         "desired_dataset_name": "brutos_informes_vitacare",
         "desired_table_name": "listagem_pacientes_tea",
+        "environment": "prod",
+        "rename_flow": True,
+    },
+    {
+        "file_pattern": "INFORMES-MENSAIS-ETSN/AP*/*/REPORT_SOLICITACAO_SAUDE_BUCAL_*/*/*SOLICITACAO_SAUDE_BUCAL_*.csv",
+        "desired_dataset_name": "brutos_informes_vitacare",
+        "desired_table_name": "solicitacao_saude_bucal",
+        "environment": "prod",
+        "rename_flow": True,
+    },
+    {
+        "file_pattern": "INFORMES-MENSAIS-ETSN/AP*/*/REPORT_SUB_PAV_OFICIO54_RELACAO_HAS_DIA/*/*_RELACAO_HASDM_VITAhisCARE.csv",
+        "desired_dataset_name": "brutos_informes_vitacare",
+        "desired_table_name": "relacao_hasdm_vitahiscare",
         "environment": "prod",
         "rename_flow": True,
     },
