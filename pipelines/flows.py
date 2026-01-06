b2e17dfd466527ed0146f4cb2e6b7a5a57c2e950
miloskimatheus <matheus.miloski@dados.rio>
2026-01-06 15:28:08 -0300

    feat: update flow imports to remove deprecated `emails_subgeral` and add `emails_subgeral_gestao`

--- pipelines/flows.py
+++ pipelines/flows.py
@@ -18,7 +18,6 @@ from pipelines.datalake.extract_load.diario_oficial_rj.flows import *
 from pipelines.datalake.extract_load.diario_oficial_uniao.flows import *
 from pipelines.datalake.extract_load.diario_oficial_uniao_api.flows import *
 from pipelines.datalake.extract_load.exames_laboratoriais_api.flows import *
-from pipelines.datalake.extract_load.emails_subgeral.flows import *
 from pipelines.datalake.extract_load.extract_gdb.flows import *
 from pipelines.datalake.extract_load.gal_gcs.flows import *
 from pipelines.datalake.extract_load.google_sheets.flows import *
@@ -68,6 +67,8 @@ from pipelines.reports.informes_seguranca.flows import *
 from pipelines.reports.ingestao_dados.flows import *
 from pipelines.reports.long_running_flows.flows import *
 from pipelines.reports.monitoramento_hci.flows import *
+from pipelines.reports.emails_subgeral_gestao.flows import *
+
 
 # ===============================
 # TOOLS
