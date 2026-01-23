a4a00e0b5e559f9a6079862e1782ffff264fec7f
Herian <herian.cavalcante@live.com>
2026-01-22 23:57:38 -0300

    fix: adjust retry settings in make_request function for improved error handling

--- pipelines/datalake/extract_load/siclom_api/utils.py
+++ pipelines/datalake/extract_load/siclom_api/utils.py
@@ -6,7 +6,7 @@ from urllib3.util.retry import Retry
 
 def make_request(url, headers):
     session = requests.Session()
-    retries = Retry(total=3, backoff_factor=5, status_forcelist=[500, 502, 503, 504, 104])
+    retries = Retry(total=2, backoff_factor=1, status_forcelist=[500, 502, 503, 504, 104])
     session.mount("https://", HTTPAdapter(max_retries=retries))
     try:
         response = session.get(url=url, headers=headers)
