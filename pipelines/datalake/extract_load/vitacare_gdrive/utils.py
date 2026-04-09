3a52170ed470e845fcc536f2566d604eb4f54e9c
Daniel Lira <danielllira@icloud.com>
2026-04-09 11:24:11 -0300

    rerun deploy

--- pipelines/datalake/extract_load/vitacare_gdrive/utils.py
+++ pipelines/datalake/extract_load/vitacare_gdrive/utils.py
@@ -169,7 +169,7 @@ def fix_RELACAO_CRIANCAS_MENORES_5_ANOS(csv_text: str, sep: str):
     '''
     Vitacare mudou o campo em um dos informes, essa funcao (quebra galho)
     corrige para que a tabela e=seja atualizada
-    TODO - Rever lógica num futuro proximo - 24 Mar 2026 hoje
+    TODO - Rever lógica num futuro proximo - 24 Mar 2026 hj
     '''
     NOME_NOVO = "N_REGISTRO_AVALIACAO_DESENVOLVIMENTO_INTEGRAL_CRIANCA"
     NOME_ANTIGO = "N_REGISTRO_AVALIACAO_DESENVOLVIMENTO_INFANTIL_DENVER"
